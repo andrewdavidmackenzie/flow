bf6f8f4d1fa27712b99f3aa70adbfff314632607
Andrew Mackenzie <andrew@mackenzie-serres.net>
2015-12-21 01:06:51 +0100

    Added a test for second sample

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,8 +2,17 @@ extern crate flow;
 use flow::parser::parser;
 
 #[test]
-fn can_load_sample() {
+fn sample_hello_world_simple() {
     let sample_path = "samples/hello-world-simple/hello.context";
+    match parser::load(sample_path, true) {
+        parser::Result::ContextLoaded(_) => {},
+        _ => assert!(false),
+    }
+}
+
+#[test]
+fn sample_hello_world() {
+    let sample_path = "samples/hello-world/hello.context";
     match parser::load(sample_path, true) {
         parser::Result::ContextLoaded(_) => {},
         _ => assert!(false),
