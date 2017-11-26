8520106feec17b70c2798047e76cc36b184b49ba
Andrew Mackenzie <andrew@mackenzie-serres.net>
2017-11-26 10:12:35 +0100

    Fix tests

--- lib/tests/samples.rs
+++ lib/tests/samples.rs
@@ -21,23 +21,23 @@ fn sample_hello_world() {
 #[test]
 fn dump_hello_simple_toml() {
     let path = PathBuf::from("../samples/hello-world-simple-toml/context.toml");
-    dump(loader::load_flow(path).unwrap(), 0);
+    dump(&loader::load_flow(path).unwrap(), 0);
 }
 
 #[test]
 fn dump_hello_world_toml_context() {
     let path = PathBuf::from("../samples/hello-world-toml/context.toml");
-    dump(loader::load_flow(path).unwrap(), 0);
+    dump(&loader::load_flow(path).unwrap(), 0);
 }
 
 #[test]
 fn dump_hello_world_flow1_toml() {
     let path = PathBuf::from("../samples/hello-world-toml/flow1.toml");
-    dump( loader::load_flow(path).unwrap(), 0);
+    dump( &loader::load_flow(path).unwrap(), 0);
 }
 
 #[test]
 fn dump_complex1() {
     let path = PathBuf::from("../samples/complex1/context.toml");
-    dump(loader::load_flow(path).unwrap(), 0);
+    dump(&loader::load_flow(path).unwrap(), 0);
 }
