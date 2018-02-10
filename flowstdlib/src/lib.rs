b2785fdc815b15d09cb3ab842d4546705f1cbbb9
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-02-10 21:30:27 +0100

    No need for a build script now I think. Include the function definition toml files in the package.

--- flowstdlib/src/lib.rs
+++ flowstdlib/src/lib.rs
@@ -3,9 +3,3 @@ extern crate flowrlib;
 pub mod stdio;
 pub mod math;
 pub mod info;
-
-include!(concat!(env!("OUT_DIR"), "/manifest.rs"));
-
-pub fn get_message()  -> &'static str {
-    message()
-}
