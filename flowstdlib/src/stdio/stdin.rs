d391575929bfe3983e29fb98afe0d04e37b8ff6a
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-02-15 03:23:54 +0100

    Add a test to load by passing only a directory name

--- flowstdlib/src/stdio/stdin.rs
+++ flowstdlib/src/stdio/stdin.rs
@@ -6,16 +6,16 @@ use std::fmt::Debug;
 pub struct Stdin;
 
 impl Implementation for Stdin {
-    fn number_of_inputs(&self) -> usize {
-        0
-    }
-
     fn run(&self, _inputs: Vec<Option<String>>) -> Option<String> {
         use std::io::{self, Read};
 
         let mut buffer = String::new();
         io::stdin().read_to_string(&mut buffer).unwrap();
         Some(buffer)
+    }
+
+    fn number_of_inputs(&self) -> usize {
+        0
     }
 
     fn name(&self) -> &'static str {
