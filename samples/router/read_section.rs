9075e91ce3247255291ad520373a4d53e4960394
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-04-02 15:41:40 +0200

    Work on a router example

---
+++ samples/router/read_section.rs
@@ -0,0 +1,24 @@
+use serde_json::Value as JsonValue;
+use serde_json::Value::String as JsonString;
+use flowrlib::implementation::Implementation;
+use flowrlib::runnable::Runnable;
+use flowrlib::runlist::RunList;
+
+pub struct ReadSection;
+
+impl Implementation for ReadSection {
+    fn run(&self, runnable: &Runnable, mut inputs: Vec<JsonValue>, run_list: &mut RunList) {
+        let input = inputs.remove(0);
+        match &input {
+            &JsonString(ref s) => {
+                let output = json!({
+                "a" : s,
+                "b" : s,
+                "c" : s
+            });
+                run_list.send_output(runnable, output);
+            },
+            _ => {}
+        }
+    }
+}
