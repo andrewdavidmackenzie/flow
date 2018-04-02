73badf4f044c3cc3b156f17c12e32bba6420a808
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-04-02 16:51:23 +0200

    Simplify reverse-echo sample

--- samples/reverse-echo/reverse.rs
+++ samples/reverse-echo/reverse.rs
@@ -9,8 +9,8 @@ pub struct Reverse;
 impl Implementation for Reverse {
     fn run(&self, runnable: &Runnable, mut inputs: Vec<JsonValue>, run_list: &mut RunList) {
         let input = inputs.remove(0);
-        match &input {
-            &JsonString(ref s) => {
+        match input {
+            JsonString(ref s) => {
                 let output = json!({
                     "reversed" : s.chars().rev().collect::<String>(),
                     "original": s
