e70160327558a04162366ac76d91203d51345c9b
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-04-17 14:48:12 +0200

    Avoid creating test output files in directory where tests are run, use a temp dir

--- flowclib/src/dumper/dumper.rs
+++ flowclib/src/dumper/dumper.rs
@@ -23,7 +23,6 @@ use model::connection::Route;
 /// use tempdir::TempDir;
 ///
 /// let mut url = url::Url::from_file_path(env::current_dir().unwrap()).unwrap();
-/// println!("url = {:?}", url);
 /// url = url.join("samples/hello-world-simple/context.toml").unwrap();
 /// let flow = flowclib::loader::loader::load(&url).unwrap();
 /// let output_dir = TempDir::new("dumper").unwrap().into_path();
@@ -242,14 +241,17 @@ fn flow_to_dot(flow_ref: &FlowReference) -> String {
 /// ```
 /// extern crate url;
 /// extern crate flowclib;
+/// extern crate tempdir;
+///
 /// use std::env;
+/// use tempdir::TempDir;
 ///
 /// let mut url = url::Url::from_file_path(env::current_dir().unwrap()).unwrap();
 /// println!("url = {:?}", url);
 /// url = url.join("samples/hello-world-simple/context.toml").unwrap();
 /// let mut flow = flowclib::loader::loader::load(&url).unwrap();
 /// let tables = flowclib::compiler::compile::compile(&mut flow).unwrap();
-/// let output_dir = env::current_dir().unwrap();
+/// let output_dir = TempDir::new("dumper").unwrap().into_path();
 ///
 /// flowclib::dumper::dumper::dump_tables(&tables, &output_dir).unwrap();
 /// ```
