baeff13d5385789adce9fed409556952ec831fe3
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-03-02 20:43:00 +0100

    remove TODO that doesn't work

--- flowrlib/src/debug_client.rs
+++ flowrlib/src/debug_client.rs
@@ -1,7 +1,6 @@
 use std::io;
 
 pub trait DebugClient {
-    // TODO change to accept FromStr ???
     fn display(&self, output: &str);
     fn read_input(&self, input: &mut String) -> io::Result<usize>;
 }
