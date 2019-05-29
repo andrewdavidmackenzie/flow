7453e592852e96fc7aa753b81ff14472814008c3
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-05-28 22:04:29 -0400

    Read a file with command line input arguments for flow line by line

--- flowc/tests/execution_tests.rs
+++ flowc/tests/execution_tests.rs
@@ -6,6 +6,7 @@ extern crate url;
 
 use std::env;
 use std::fs::File;
+use std::io::BufReader;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::Command;
@@ -56,10 +57,11 @@ fn write_manifest(flow: &Flow, debug_symbols: bool, out_dir: PathBuf, tables: &G
     Ok(filename)
 }
 
-fn execute_flow(run_dir: PathBuf, filepath: PathBuf) -> Output {
+fn execute_flow(run_dir: PathBuf, filepath: PathBuf, test_args: Vec<String>) -> Output {
     let mut command = Command::new("cargo");
-    let command_args = vec!("run", "--bin", "flowr", filepath.to_str().unwrap(),
-                            "--", "arg1");
+    let mut command_args = vec!("run", "--bin", "flowr", filepath.to_str().unwrap(),
+                            "--");
+    test_args.iter().map(|arg| command_args.push(arg));
     command.current_dir(run_dir);
     command.args(command_args)
         .stdin(Stdio::inherit())
@@ -68,18 +70,32 @@ fn execute_flow(run_dir: PathBuf, filepath: PathBuf) -> Output {
         .output().unwrap()
 }
 
-fn test_args(test_name: &str) -> Vec<&str> {
+fn test_args(test_dir: &PathBuf, test_name: &str) -> Vec<String> {
     let test_args = format!("{}.args", test_name);
+    let mut args_file = test_dir.clone();
+    args_file.push(test_args);
+    let f = File::open(&args_file).unwrap();
+    let f = BufReader::new(f);
 
-    // TODO read args from file
-
-    vec!()
+    let mut args = Vec::new();
+    for line in f.lines() {
+        args.push(line.unwrap());
+    }
+    args
 }
 
-fn load_flow(test_dir: &mut PathBuf, test_name: &str) -> Process {
+fn load_flow(test_dir: &PathBuf, test_name: &str) -> Process {
     let test_flow = format!("{}.toml", test_name);
-    test_dir.push(test_flow);
-    loader::load_context(&url_from_rel_path(&test_dir.to_string_lossy()), &MetaProvider {}).unwrap()
+    let mut flow_file = test_dir.clone();
+    flow_file.push(test_flow);
+    loader::load_context(&url_from_rel_path(&flow_file.to_string_lossy()), &MetaProvider {}).unwrap()
+}
+
+fn get(file_path: &PathBuf) -> Vec<u8> {
+    let mut f = File::open(&file_path).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+    buffer
 }
 
 fn execute_test(test_name: &str) {
@@ -88,14 +104,15 @@ fn execute_test(test_name: &str) {
     let mut run_dir = test_dir.clone();
     run_dir.pop();
     test_dir.push("tests/samples");
-    let out_dir = test_dir.clone();
-    let test_stdin = format!("{}.stdin", test_name);
-    let test_args = test_args(test_name);
 
-    if let FlowProcess(ref flow) = load_flow(&mut test_dir, test_name) {
+    if let FlowProcess(ref flow) = load_flow(&test_dir, test_name) {
         let tables = compile::compile(flow).unwrap();
+        let out_dir = test_dir.clone();
         let manifest_path = write_manifest(flow, true, out_dir, &tables).unwrap();
-        let output = execute_flow(run_dir, manifest_path);
+
+        let test_stdin = format!("{}.stdin", test_name);
+        let mut test_args = test_args(&test_dir, test_name);
+        let output = execute_flow(run_dir, manifest_path, test_args);
     }
 }
 
