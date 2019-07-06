a21237ccbf1a0581768ebe031f1afa16c6607ef0
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-07-06 19:01:32 -0400

    Rename some variables

--- ide/app/main.js
+++ ide/app/main.js
@@ -2,13 +2,12 @@ const {app, BrowserWindow} = require('electron');
 //const path = require('path');
 //const url = require('url');
 
-// Keep a global reference of the window object, if you don't, the window will
+// Keep a global reference of the main window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let mainWindow;
 
-function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({
+function createUI() {
+    mainWindow = new BrowserWindow({
         width: 1400, height: 900,
         nodeIntegration: true
     });
@@ -24,22 +23,22 @@ function createWindow() {
         });
     });
 
-    win.loadFile("index.html");
+    mainWindow.loadFile("index.html");
 
-    win.webContents.openDevTools({mode: 'bottom'});
+    mainWindow.webContents.openDevTools({mode: 'bottom'});
 
-    win.on('closed', () => {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
+        mainWindow = null
     });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createUI);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -53,8 +52,8 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
+    if (mainWindow === null) {
+        createUI()
     }
 });
 
