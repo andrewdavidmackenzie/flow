4e8d3e098aca722d80ee6abd41156e73efceaba0
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-07-06 21:23:00 -0400

    Trying to debug css/js folder issues without much success

--- ide/app/renderer.js
+++ ide/app/renderer.js
@@ -5,6 +5,7 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
+        console.log("click");
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
