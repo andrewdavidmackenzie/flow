6460363eb65908b00c2e7b3f34b01ac29d0a5da2
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-09-04 20:44:50 +0100

    Avoid number overflow in Add

--- flowstdlib/math/add/add.rs
+++ flowstdlib/math/add/add.rs
@@ -11,7 +11,6 @@ use serde_json::Value::Number;
 use serde_json::Value::String;
 
 #[derive(FlowImpl)]
-
 pub struct Add;
 
 // TODO implementation of `std::ops::Add` might be missing for `&serde_json::Number`
@@ -24,24 +23,17 @@ impl Implementation for Add {
         let input_b = inputs.get(1).unwrap();
         match (&input_a[0], &input_b[0]) {
             (&Number(ref a), &Number(ref b)) => {
-                // TODO mixed signed and unsigned integers
-                if a.is_i64() && b.is_i64() {
-                    value = Some(Value::Number(serde_json::Number::from(a.as_i64().unwrap() + b.as_i64().unwrap())));
-                } else if a.is_u64() && b.is_u64() {
-                    value = Some(Value::Number(serde_json::Number::from(a.as_u64().unwrap() + b.as_u64().unwrap())));
-                } else if a.is_f64() || b.is_f64() {
-                    value = Some(Value::Number(serde_json::Number::from_f64(a.as_f64().unwrap() + b.as_f64().unwrap()).unwrap()));
-                }
+                value = Some(Value::Number(serde_json::Number::from(a.as_i64().unwrap().checked_add(b.as_i64().unwrap()).unwrap())));
             }
             (&String(ref a), &String(ref b)) => {
-                let i1 = a.parse::<i32>().unwrap();
-                let i2 = b.parse::<i32>().unwrap();
+                let i1 = a.parse::<i64>().unwrap();
+                let i2 = b.parse::<i64>().unwrap();
                 let o1 = i1 + i2;
                 value = Some(Value::String(o1.to_string()));
             }
             (_, _) => {}
         }
 
-        (value, true)
+        (value, RUN_AGAIN)
     }
 }
