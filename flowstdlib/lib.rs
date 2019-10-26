baddcc8ff7566b2e05bc5fad2329a34adf8bd946
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-10-26 16:47:41 +0200

    Made statically compiled version of flowstdlib a "feature" of the library - on by default

--- flowstdlib/lib.rs
+++ flowstdlib/lib.rs
@@ -3,18 +3,28 @@
 //! It can be compiled and linked natively to a runtime, or each function can be
 //! compiled to WebAssembly and loaded from file by the runtime.
 
+#[cfg(feature = "static")]
 /// Use serde_json for data representations of Values passed to/from functions
 #[macro_use]
 extern crate serde_json;
 
+#[cfg(feature = "static")]
 /// Control functions
 pub mod control;
+
+#[cfg(feature = "static")]
 /// Data functions
 pub mod data;
+
+#[cfg(feature = "static")]
 /// Formatting functions
 pub mod fmt;
+
+#[cfg(feature = "static")]
 /// Imaging functions
 pub mod img;
+
+#[cfg(feature = "static")]
 /// Maths functions
 pub mod math;
 
