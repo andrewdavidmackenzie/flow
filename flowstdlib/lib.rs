e39e4d63c8cc256c74017aa9e7ff58ba8db43521
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-11-03 21:49:27 +0100

    Reorder manifest

--- flowstdlib/lib.rs
+++ flowstdlib/lib.rs
@@ -47,34 +47,43 @@ pub fn get_manifest() -> LibraryManifest {
     };
     let mut manifest = LibraryManifest::new(metadata);
 
-    manifest.locators.insert("lib://flowstdlib/data/zip/Zip".to_string(),
-                             Native(Arc::new(data::zip::zip::Zip)));
-    manifest.locators.insert("lib://flowstdlib/fmt/to_string/ToString".to_string(),
-                             Native(Arc::new(fmt::to_string::to_string::ToString)));
-    manifest.locators.insert("lib://flowstdlib/img/format_png/FormatPNG".to_string(),
-                             Native(Arc::new(img::format_png::format_png::FormatPNG)));
-    manifest.locators.insert("lib://flowstdlib/math/divide/Divide".to_string(),
-                             Native(Arc::new(math::divide::divide::Divide)));
+    // Compare module
+    manifest.locators.insert("lib://flowstdlib/control/compare/Compare".to_string(),
+                             Native(Arc::new(control::compare::compare::Compare)));
     manifest.locators.insert("lib://flowstdlib/control/join/Join".to_string(),
                              Native(Arc::new(control::join::join::Join)));
     manifest.locators.insert("lib://flowstdlib/control/tap/Tap".to_string(),
                              Native(Arc::new(control::tap::tap::Tap)));
+
+    // Data module
+    manifest.locators.insert("lib://flowstdlib/data/buffer/Buffer".to_string(),
+                             Native(Arc::new(data::buffer::buffer::Buffer)));
+    manifest.locators.insert("lib://flowstdlib/data/compose_array/ComposeArray".to_string(),
+                             Native(Arc::new(data::compose_array::compose_array::ComposeArray)));
+    manifest.locators.insert("lib://flowstdlib/data/zip/Zip".to_string(),
+                             Native(Arc::new(data::zip::zip::Zip)));
+
+    // Fmt module
     manifest.locators.insert("lib://flowstdlib/fmt/reverse/Reverse".to_string(),
                              Native(Arc::new(fmt::reverse::reverse::Reverse)));
     manifest.locators.insert("lib://flowstdlib/fmt/to_number/ToNumber".to_string(),
                              Native(Arc::new(fmt::to_number::to_number::ToNumber)));
+    manifest.locators.insert("lib://flowstdlib/fmt/to_string/ToString".to_string(),
+                             Native(Arc::new(fmt::to_string::to_string::ToString)));
+
+    // Img module
+    manifest.locators.insert("lib://flowstdlib/img/format_png/FormatPNG".to_string(),
+                             Native(Arc::new(img::format_png::format_png::FormatPNG)));
+
+    // Math module
     manifest.locators.insert("lib://flowstdlib/math/add/Add".to_string(),
                              Native(Arc::new(math::add::add::Add)));
-    manifest.locators.insert("lib://flowstdlib/control/compare/Compare".to_string(),
-                             Native(Arc::new(control::compare::compare::Compare)));
-    manifest.locators.insert("lib://flowstdlib/math/subtract/Subtract".to_string(),
-                             Native(Arc::new(math::subtract::subtract::Subtract)));
+    manifest.locators.insert("lib://flowstdlib/math/divide/Divide".to_string(),
+                             Native(Arc::new(math::divide::divide::Divide)));
     manifest.locators.insert("lib://flowstdlib/math/sqrt/Sqrt".to_string(),
                              Native(Arc::new(math::sqrt::sqrt::Sqrt)));
-    manifest.locators.insert("lib://flowstdlib/data/compose_array/ComposeArray".to_string(),
-                             Native(Arc::new(data::compose_array::compose_array::ComposeArray)));
-    manifest.locators.insert("lib://flowstdlib/data/buffer/Buffer".to_string(),
-                             Native(Arc::new(data::buffer::buffer::Buffer)));
+    manifest.locators.insert("lib://flowstdlib/math/subtract/Subtract".to_string(),
+                             Native(Arc::new(math::subtract::subtract::Subtract)));
 
     manifest
 }
