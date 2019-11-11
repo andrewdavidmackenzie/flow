c11fa6717a77f6a67b27a6e73e01925f8c59d2d4
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-11-11 23:02:43 +0100

    Load runtime and flowtdlib libraries at startup

--- flowide/src/main.rs
+++ flowide/src/main.rs
@@ -6,12 +6,16 @@ extern crate flowclib;
 extern crate flowrlib;
 extern crate gio;
 extern crate gtk;
+extern crate provider;
 #[macro_use]
 extern crate serde_json;
 
+use flowrlib::loader::Loader;
+use flowrlib::provider::Provider;
 use gio::prelude::*;
 use gtk::{Application, ApplicationWindow, Label};
 use gtk::prelude::*;
+use provider::content::provider::MetaProvider;
 use std::env::args;
 
 mod runtime;
@@ -30,6 +34,16 @@ fn build_ui(app: &gtk::Application) {
     window.show_all();
 }
 
+fn load_libs(loader: &mut Loader, provider: &dyn Provider) -> Result<(), String> {
+    // Load this runtime's library of native (statically linked) implementations
+    loader.add_lib(provider, runtime::manifest::get_manifest(), "runtime")
+        .map_err(|e| e.to_string())?;
+
+    // If the "native" feature is enabled then load the native flowstdlib if command line arg to do so
+    loader.add_lib(provider, flowstdlib::get_manifest(), "flowstdlib")
+        .map_err(|e| e.to_string())
+}
+
 fn main() {
     let application = Application::new(
         Some("net.mackenzie-serres.flow.ide"),
@@ -39,6 +53,11 @@ fn main() {
     application.connect_activate(|app| {
         build_ui(app);
     });
+
+    let mut loader = Loader::new();
+    let provider = MetaProvider {};
+
+    let _result = load_libs(&mut loader, &provider);
 
     application.run(&args().collect::<Vec<_>>());
 }
