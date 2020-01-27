e359348e24bcb31f0a765a4dfee812c2fd98eeb5
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-01-27 21:42:20 +0100

    Increase test coverage of LibraryManifest

--- flowrlib/src/lib_manifest.rs
+++ flowrlib/src/lib_manifest.rs
@@ -112,6 +112,11 @@ impl PartialEq for LibraryManifest {
 
 #[cfg(test)]
 mod test {
+    use std::sync::Arc;
+
+    use flow_impl::Implementation;
+    use serde_json::Value;
+
     use crate::errors::*;
     use crate::lib_manifest::{ImplementationLocator, ImplementationLocator::Wasm, LibraryManifest};
     use crate::manifest::MetaData;
@@ -119,6 +124,26 @@ mod test {
 
     pub struct TestProvider {
         test_content: &'static str
+    }
+
+    fn test_meta_data() -> MetaData {
+        MetaData {
+            name: "test".into(),
+            version: "0.0.0".into(),
+            description: "a test".into(),
+            author_name: "me".into(),
+            author_email: "me@a.com".into(),
+        }
+    }
+
+    fn test_meta_data2() -> MetaData {
+        MetaData {
+            name: "different".into(),
+            version: "0.0.0".into(),
+            description: "a test".into(),
+            author_name: "me".into(),
+            author_email: "me@a.com".into(),
+        }
     }
 
     impl Provider for TestProvider {
@@ -129,6 +154,41 @@ mod test {
         fn get_contents(&self, _url: &str) -> Result<Vec<u8>> {
             Ok(self.test_content.as_bytes().to_owned())
         }
+    }
+
+    #[test]
+    fn create() {
+        let _ = LibraryManifest::new(test_meta_data());
+    }
+
+    #[test]
+    fn wasm_locators_match() {
+        let loc0 = ImplementationLocator::Wasm("location".into());
+        let loc1 = ImplementationLocator::Wasm("location".into());
+
+        assert!(loc0 == loc1);
+    }
+
+    #[test]
+    fn wasm_locators_dont_match() {
+        let loc0 = ImplementationLocator::Wasm("location0".into());
+        let loc1 = ImplementationLocator::Wasm("location1".into());
+
+        assert!(loc0 != loc1);
+    }
+
+    #[test]
+    fn locators_type_mismatch() {
+        struct TestImpl {};
+        impl Implementation for TestImpl {
+            fn run(&self, _inputs: Vec<Vec<Value>>) -> (Option<Value>, bool) {
+                unimplemented!()
+            }
+        }
+        let wasm_loc = ImplementationLocator::Wasm("wasm_location".into());
+        let native_loc = ImplementationLocator::Native(Arc::new(TestImpl{}));
+
+        assert!(wasm_loc != native_loc);
     }
 
     #[test]
@@ -186,6 +246,53 @@ mod test {
             Wasm(source) => assert_eq!(source, "add2.wasm"),
             _ => assert!(false, "Expected type 'Wasm' but found another type")
         }
+    }
+
+    #[test]
+    fn add_to() {
+        let mut library = LibraryManifest::new(test_meta_data());
+        library.add_to_manifest( "/fake", "/bin/my.wasm", "/bin", "my function");
+        assert_eq!(library.locators.len(), 1, "There should be one implementation location in the library manifest");
+    }
+
+    #[test]
+    fn compare_manifests_metadata_different() {
+        let library1 = LibraryManifest::new(test_meta_data());
+        let library2 = LibraryManifest::new(test_meta_data2());
+
+        assert!(library1 != library2);
+    }
+
+    #[test]
+    fn compare_manifests_num_locators_different() {
+        let mut library1 = LibraryManifest::new(test_meta_data());
+        library1.add_to_manifest( "/fake", "/bin/my.wasm", "/bin", "my function");
+
+        let library2 = LibraryManifest::new(test_meta_data());
+
+        assert!(library1 != library2);
+    }
+
+    #[test]
+    fn compare_manifests_locators_different() {
+        let mut library1 = LibraryManifest::new(test_meta_data());
+        library1.add_to_manifest( "/fake", "/bin/fake.wasm", "/bin", "my fake function");
+
+        let mut library2 = LibraryManifest::new(test_meta_data());
+        library2.add_to_manifest( "/different", "/bin/my.wasm", "/bin", "my function");
+
+        assert!(library1 != library2);
+    }
+
+    #[test]
+    fn compare_manifests_same() {
+        let mut library1 = LibraryManifest::new(test_meta_data());
+        library1.add_to_manifest( "/fake", "/bin/my.wasm", "/bin", "my function");
+
+        let mut library2 = LibraryManifest::new(test_meta_data());
+        library2.add_to_manifest( "/fake", "/bin/my.wasm", "/bin", "my function");
+
+        assert!(library1 == library2);
     }
 }
 
