8bddaf0bfdeeaad7b38d9a0209e3381befcadbab
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-01-29 19:55:39 +0100

    Use FLOW_LIB_PATH in test helper to find test files

--- flowc/tests/helper.rs
+++ flowc/tests/helper.rs
@@ -4,8 +4,7 @@ use std::path::PathBuf;
 use url::Url;
 
 pub fn absolute_file_url_from_relative_path(path: &str) -> String {
-    let mut flow_root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    flow_root.pop();
+    let flow_root = PathBuf::from(env::var("FLOW_LIB_PATH").unwrap());
     let abs_url = Url::from_directory_path(flow_root).unwrap().join(path).unwrap().to_string();
     abs_url
 }
