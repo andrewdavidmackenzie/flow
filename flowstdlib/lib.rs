b789411e83055646d9bb2f505bde2e215daa5af4
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-02-02 12:33:41 +0100

    Simplify .gitignores and remove unneeded ones

--- flowstdlib/lib.rs
+++ flowstdlib/lib.rs
@@ -3,35 +3,27 @@
 //! It can be compiled and linked natively to a run-time, or each function can be
 //! compiled to WebAssembly and loaded from file by the run-time.
 
-#[cfg(feature = "native")]
 use std::sync::Arc;
 
-#[cfg(feature = "native")]
 use flowrlib::lib_manifest::{ImplementationLocator::Native, LibraryManifest};
-#[cfg(feature = "native")]
+
 use flowrlib::manifest::MetaData;
 
-#[cfg(feature = "native")]
 /// Control functions
 pub mod control;
 
-#[cfg(feature = "native")]
 /// Data functions
 pub mod data;
 
-#[cfg(feature = "native")]
 /// Formatting functions
 pub mod fmt;
 
-#[cfg(feature = "native")]
 /// Imaging functions
 pub mod img;
 
-#[cfg(feature = "native")]
 /// Maths functions
 pub mod math;
 
-#[cfg(feature = "native")]
 /// Return the `LibraryManifest` for the functions in the library
 pub fn get_manifest() -> LibraryManifest {
     let metadata = MetaData {
