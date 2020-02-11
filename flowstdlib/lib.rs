598c97f303e202685f0a9c93f14da162b751fe2f
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-02-10 20:22:21 -0500

    Add compare-switch function

--- flowstdlib/lib.rs
+++ flowstdlib/lib.rs
@@ -6,7 +6,6 @@
 use std::sync::Arc;
 
 use flowrlib::lib_manifest::{ImplementationLocator::Native, LibraryManifest};
-
 use flowrlib::manifest::MetaData;
 
 /// Control functions
@@ -56,6 +55,8 @@ pub fn get_manifest() -> LibraryManifest {
                              Native(Arc::new(math::add::add::Add)));
     manifest.locators.insert("lib://flowstdlib/control/compare/Compare".to_string(),
                              Native(Arc::new(control::compare::compare::Compare)));
+    manifest.locators.insert("lib://flowstdlib/control/compare/CompareSwitch".to_string(),
+                             Native(Arc::new(control::compare_switch::compare_switch::CompareSwitch)));
     manifest.locators.insert("lib://flowstdlib/math/subtract/Subtract".to_string(),
                              Native(Arc::new(math::subtract::subtract::Subtract)));
     manifest.locators.insert("lib://flowstdlib/data/compose_array/ComposeArray".to_string(),
