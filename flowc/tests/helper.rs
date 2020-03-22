1fca9d7e16715dbea04f55b7168ac0e251e60024
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-03-22 19:40:16 +0100

    Print a message and try setting FLOW_LIB_PATH to propject root if not already set

--- flowc/tests/helper.rs
+++ flowc/tests/helper.rs
@@ -4,7 +4,17 @@ use std::path::PathBuf;
 use url::Url;
 
 pub fn absolute_file_url_from_relative_path(path: &str) -> String {
-    let flow_root = PathBuf::from(env::var("FLOW_LIB_PATH").unwrap());
+    if env::var("FLOW_LIB_PATH").is_err() {
+        eprint!("Environment Variable 'FLOW_LIB_PATH' is not set, so cannot find libraries for testing\n");
+        eprint!("In development you can set 'FLOW_LIB_PATH' to the root directory of the project and that should work.\n");
+        let cwd = env::current_dir().unwrap();
+        let project_root = cwd.parent().unwrap();
+        eprint!("Setting 'FLOW_LIB_PATH' to '{}'\n", project_root.display());
+        env::set_var("FLOW_LIB_PATH", project_root)
+    }
+
+    let value = env::var("FLOW_LIB_PATH").unwrap();
+    let flow_root = PathBuf::from(value);
     let abs_url = Url::from_directory_path(flow_root).unwrap().join(path).unwrap().to_string();
     abs_url
 }
