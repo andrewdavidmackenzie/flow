03c7dd89ea89f41fe637b85aeef1c1575e60d117
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-03-30 19:04:21 +0200

    Add an info function to flowstdlib that gives you information about a value.

--- flowstdlib/data/mod.rs
+++ flowstdlib/data/mod.rs
@@ -1,15 +1,17 @@
 //! ## Control (//flowstdlib/data)
 //! Some generic Processes that act on data.
 
+/// A module that implements accumulating an array from a number of elements
+pub mod accumulate;
 /// A module that implements a data buffer
 pub mod buffer;
 /// A module that implements composing an array from a number of elements
 pub mod compose_array;
-/// A module that implements accumulating an array from a number of elements
-pub mod accumulate;
-/// A module that zips two sets of data into a set of tuples of data
-pub mod zip;
+/// A module with a function to get info about a Value
+pub mod info;
 /// A module that removes elements from an array
 pub mod remove;
 /// A module with a function for transposing a Matrix
 pub mod transpose;
+/// A module that zips two sets of data into a set of tuples of data
+pub mod zip;
--- flowstdlib/lib.rs
+++ flowstdlib/lib.rs
@@ -57,6 +57,8 @@ pub fn get_manifest() -> LibraryManifest {
                              Native(Arc::new(data::buffer::buffer::Buffer)));
     manifest.locators.insert("lib://flowstdlib/data/compose_array/ComposeArray".to_string(),
                              Native(Arc::new(data::compose_array::compose_array::ComposeArray)));
+    manifest.locators.insert("lib://flowstdlib/data/info/Info".to_string(),
+                             Native(Arc::new(data::info::info::Info)));
     manifest.locators.insert("lib://flowstdlib/data/remove/Remove".to_string(),
                              Native(Arc::new(data::remove::remove::Remove)));
     manifest.locators.insert("lib://flowstdlib/data/transpose/Transpose".to_string(),
