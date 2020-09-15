ac63f36bc71ec654b8ae6487835dd5c3a168fd99
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-09-15 16:31:14 +0200

    Comments changes in flow.rs - still have to fix selecting array elements fron output sub-routes

--- flowc/src/model/flow.rs
+++ flowc/src/model/flow.rs
@@ -89,7 +89,7 @@ impl Validate for Flow {
 impl fmt::Display for Flow {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "\tname: \t\t\t{}\n\tid: \t\t\t{}\n\talias: \t\t\t{}\n\tsource_url: \t{}\n\troute: \t\t\t{}",
-               self.name, self.id, self.alias, self.source_url, self.route).unwrap();
+                 self.name, self.id, self.alias, self.source_url, self.route).unwrap();
 
         writeln!(f, "\tinputs:").unwrap();
         if let Some(ref inputs) = self.inputs {
@@ -249,29 +249,25 @@ impl Flow {
     // Then from the object find the IO (by name or route, probably route) in common code, maybe using IOSet directly?
     pub fn get_route_and_type(&mut self, direction: Direction, route: &Route,
                               initial_value: &Option<InputInitializer>) -> Result<IO> {
-        let (output_route, index, array_route) = route.without_trailing_array_index();
-        let segments: Vec<&str> = output_route.split('/').collect();
+        let segments: Vec<&str> = route.split('/').collect();
+        // TODO move this into validation of route when it is deserialized?
         if segments.is_empty() {
             bail!("Invalid connection {:?} '{}'", direction, route);
         }
 
         debug!("Looking for connection {:?} '{}'", direction, route);
-
         match (&direction, segments[0]) {
             (&Direction::FROM, "input")  if segments.len() == 2 => {
                 self.inputs.find_by_name(&Name::from(segments[1]), &None)
-            } // an input to this flow
+            }
             (&Direction::TO, "output") if segments.len() == 2 => {
                 self.outputs.find_by_name(&Name::from(segments[1]), initial_value)
-            } // an output from this flow
-            (&Direction::TO, _) | (&Direction::FROM, _) => {
-                let sub_route = if array_route {
-                    Route::from(format!("{}{}", segments[1..].join("/"), index))
-                } else {
-                    Route::from(segments[1..].join("/"))
-                };
-                self.get_io_subprocess(&Name::from(segments[0]), direction, &sub_route, initial_value)
-            } // input or output of a sub-process
+            }
+            (&Direction::TO, process_name) | (&Direction::FROM, process_name) => {
+                let sub_route = Route::from(segments[1..].join("/"));
+                self.get_io_subprocess(&Name::from(process_name), direction,
+                                       &sub_route, initial_value)
+            }
         }
     }
 
@@ -306,6 +302,9 @@ impl Flow {
                     match self.get_route_and_type(TO, &connection.to, from_io.get_initializer()) {
                         Ok(to_io) => {
                             debug!("Found connection destination:\n{:#?}", to_io);
+                            // TODO here we are only checking compatible data types from the overall FROM IO
+                            // not from sub-types in it selected via a sub-route e.g. Array/String --> String
+                            // We'd need to make compatible_types more complex and take the from sub-Route
                             if Connection::compatible_types(&from_io.datatype(), &to_io.datatype()) {
                                 debug!("Connection built from '{}' to '{}' with runtime conversion ''", from_io.route(), to_io.route());
                                 connection.from_io = from_io;
