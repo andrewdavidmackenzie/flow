decbaf6c146f66cccbfea3f9a98acc906577d8e1
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-11-14 17:11:13 +0100

    Add buil checks for FLOW_LIB_PATH in flowstdlib build script

--- flowstdlib/build.rs
+++ flowstdlib/build.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 use std::process::{Command, Stdio};
 
+use simpath::Simpath;
+
 // Build script to compile the flowstdlib WASM files and generate manifest - using flowc
 fn main() {
     let flowc = Path::new(env!("CARGO_MANIFEST_DIR")).join("../target/debug/flowc");
@@ -16,5 +18,23 @@ fn main() {
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit())
             .spawn().unwrap();
+    }
+
+    check_flow_lib_path();
+}
+
+fn check_flow_lib_path() {
+    let parent = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap().display().to_string();
+    match std::env::var("FLOW_LIB_PATH") {
+        Err(_) => {
+            println!("cargo:warning='FLOW_LIB_PATH' is not set, so 'flowstdlib' will not be found by 'flowc' or 'flowr'. Set it to an appropriate value thus: export FLOW_LIB_PATH=\"{}\"", parent);
+        }
+        Ok(value) => {
+            let lib_path = Simpath::new("FLOW_LIB_PATH");
+            if !lib_path.contains(&parent) {
+                println!("cargo:warning='FLOW_LIB_PATH' is set to '{}'. But it does not contain the directory where 'flowstdlib' is, so 'flowstdlib' will not be found by 'flowc' or 'flowr'. Add an entry for this directory thus: export FLOW_LIB_PATH=\"{}:$FLOW_LIB_PATH\"",
+                         value, parent);
+            }
+        }
     }
 }
