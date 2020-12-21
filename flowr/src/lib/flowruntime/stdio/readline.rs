782df868a86da11365fb1dd274c134fc1a802c76
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-12-21 16:43:03 +0100

    Output text from "/text" route from readline and stdin

--- flowr/src/lib/flowruntime/stdio/readline.rs
+++ flowr/src/lib/flowruntime/stdio/readline.rs
@@ -17,8 +17,14 @@ impl Implementation for Readline {
     fn run(&self, _inputs: &[Value]) -> (Option<Value>, RunAgain) {
         if let Ok(mut server) = self.server_context.lock() {
             return match server.send_event(Event::GetLine) {
-                Ok(Response::Line(contents)) => (Some(Value::String(contents)), RUN_AGAIN),
-                Ok(Response::GetLineEOF) => (Some(Value::Null), DONT_RUN_AGAIN),
+                Ok(Response::Line(contents)) => {
+                    let mut output_map = serde_json::Map::new();
+                    output_map.insert("text".into(), Value::String(contents));
+                    (Some(Value::Object(output_map)), RUN_AGAIN)
+                },
+                Ok(Response::GetLineEOF) => {
+                    (Some(Value::Null), DONT_RUN_AGAIN)
+                },
                 _ => (None, DONT_RUN_AGAIN)
             };
         }
--- flowr/src/lib/flowruntime/stdio/stdin.rs
+++ flowr/src/lib/flowruntime/stdio/stdin.rs
@@ -17,8 +17,14 @@ impl Implementation for Stdin {
     fn run(&self, _inputs: &[Value]) -> (Option<Value>, RunAgain) {
         if let Ok(mut server) = self.server_context.lock() {
             return match server.send_event(Event::GetStdin) {
-                Ok(Response::Stdin(contents)) => (Some(Value::String(contents)), RUN_AGAIN),
-                Ok(Response::GetStdinEOF) => (Some(Value::Null), DONT_RUN_AGAIN),
+                Ok(Response::Stdin(contents)) => {
+                    let mut output_map = serde_json::Map::new();
+                    output_map.insert("text".into(), Value::String(contents));
+                    (Some(Value::Object(output_map)), RUN_AGAIN)
+                },
+                Ok(Response::GetStdinEOF) => {
+                    (Some(Value::Null), DONT_RUN_AGAIN)
+                },
                 _ => (None, DONT_RUN_AGAIN)
             }
         }
