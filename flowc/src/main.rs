384d7a914dc92e05455044b54bf74eb1c522cc4d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-03-29 10:48:57 +0200

    Avoid showing/accepting -g debug option if debugger feature is not enabled.

--- flowc/src/main.rs
+++ flowc/src/main.rs
@@ -27,9 +27,9 @@ use provider::content::provider::MetaProvider;
 use crate::flow_compile::compile_and_execute_flow;
 use crate::lib_build::build_lib;
 
-mod source_arg;
-mod lib_build;
 mod flow_compile;
+mod lib_build;
+mod source_arg;
 
 // We'll put our errors in an `errors` module, and other modules in this crate will
 // `use crate::errors::*;` to get access to everything `error_chain!` creates.
@@ -62,7 +62,7 @@ pub struct Options {
     provided_implementations: bool,
     output_dir: PathBuf,
     stdin_file: Option<String>,
-    lib_dirs: Vec<String>
+    lib_dirs: Vec<String>,
 }
 
 fn main() {
@@ -91,13 +91,13 @@ fn main() {
 }
 
 /*
-    For the lib provider, libraries maybe installed in multiple places in the file system.
-    In order to find the content, a FLOW_LIB_PATH environment variable can be configured with a
-    list of directories in which to look for the library in question.
+   For the lib provider, libraries maybe installed in multiple places in the file system.
+   In order to find the content, a FLOW_LIB_PATH environment variable can be configured with a
+   list of directories in which to look for the library in question.
 
-    Using the "FLOW_LIB_PATH" environment variable attempt to locate the library's root folder
-    in the file system.
- */
+   Using the "FLOW_LIB_PATH" environment variable attempt to locate the library's root folder
+   in the file system.
+*/
 pub fn set_lib_search_path(search_path_additions: &[String]) -> Result<Simpath> {
     let mut lib_search_path = Simpath::new_with_separator("FLOW_LIB_PATH", ',');
 
@@ -137,81 +137,110 @@ fn run() -> Result<String> {
     Parse the command line arguments using clap
 */
 fn get_matches<'a>() -> ArgMatches<'a> {
-    App::new(env!("CARGO_PKG_NAME"))
+    let app = App::new(env!("CARGO_PKG_NAME"))
         .setting(AppSettings::TrailingVarArg)
         .version(env!("CARGO_PKG_VERSION"))
-        .arg(Arg::with_name("skip")
-            .short("s")
-            .long("skip")
-            .help("Skip execution of flow"))
-        .arg(Arg::with_name("lib")
-            .short("l")
-            .long("lib")
-            .help("Compile a flow library"))
-        .arg(Arg::with_name("lib_dir")
-            .short("L")
-            .long("libdir")
-            .number_of_values(1)
-            .multiple(true)
-            .value_name("LIB_DIR|BASE_URL")
-            .help("Add a directory or base Url to the Library Search path"))
-        .arg(Arg::with_name("dump")
-            .short("d")
-            .long("dump")
-            .help("Dump the flow to .dump files after loading it"))
-        .arg(Arg::with_name("symbols")
+        .arg(
+            Arg::with_name("skip")
+                .short("s")
+                .long("skip")
+                .help("Skip execution of flow"),
+        )
+        .arg(
+            Arg::with_name("lib")
+                .short("l")
+                .long("lib")
+                .help("Compile a flow library"),
+        )
+        .arg(
+            Arg::with_name("lib_dir")
+                .short("L")
+                .long("libdir")
+                .number_of_values(1)
+                .multiple(true)
+                .value_name("LIB_DIR|BASE_URL")
+                .help("Add a directory or base Url to the Library Search path"),
+        )
+        .arg(
+            Arg::with_name("dump")
+                .short("d")
+                .long("dump")
+                .help("Dump the flow to .dump files after loading it"),
+        )
+        .arg(
+            Arg::with_name("provided")
+                .short("p")
+                .long("provided")
+                .help("Provided function implementations should NOT be compiled from source"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .value_name("OUTPUT_DIR")
+                .help("Specify the output directory for generated manifest"),
+        )
+        .arg(
+            Arg::with_name("verbosity")
+                .short("v")
+                .long("verbosity")
+                .takes_value(true)
+                .value_name("VERBOSITY_LEVEL")
+                .help("Set verbosity level for output (trace, debug, info, warn, error (default))"),
+        )
+        .arg(
+            Arg::with_name("stdin")
+                .short("i")
+                .long("stdin")
+                .takes_value(true)
+                .value_name("STDIN_FILENAME")
+                .help("Read STDIN from the named file"),
+        )
+        .arg(
+            Arg::with_name("FLOW")
+                .help("the name of the 'flow' definition file to compile")
+                .required(false)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("flow_args")
+                .help("Arguments that will get passed onto the flow if it is executed")
+                .multiple(true),
+        );
+
+    #[cfg(feature = "debugger")]
+    let app = app.arg(
+        Arg::with_name("symbols")
             .short("g")
             .long("symbols")
-            .help("Generate debug symbols (like process names and full routes)"))
-        .arg(Arg::with_name("provided")
-            .short("p")
-            .long("provided")
-            .help("Provided function implementations should NOT be compiled from source"))
-        .arg(Arg::with_name("output")
-            .short("o")
-            .long("output")
-            .takes_value(true)
-            .value_name("OUTPUT_DIR")
-            .help("Specify the output directory for generated manifest"))
-        .arg(Arg::with_name("verbosity")
-            .short("v")
-            .long("verbosity")
-            .takes_value(true)
-            .value_name("VERBOSITY_LEVEL")
-            .help("Set verbosity level for output (trace, debug, info, warn, error (default))"))
-        .arg(Arg::with_name("stdin")
-            .short("i")
-            .long("stdin")
-            .takes_value(true)
-            .value_name("STDIN_FILENAME")
-            .help("Read STDIN from the named file"))
-        .arg(Arg::with_name("FLOW")
-            .help("the name of the 'flow' definition file to compile")
-            .required(false)
-            .index(1))
-        .arg(Arg::with_name("flow_args")
-            .help("Arguments that will get passed onto the flow if it is executed")
-            .multiple(true))
-        .get_matches()
+            .help("Generate debug symbols (like process names and full routes)"),
+    );
+
+    app.get_matches()
 }
 
 /*
     Parse the command line arguments
 */
 fn parse_args(matches: ArgMatches) -> Result<Options> {
-    let mut flow_args: Vec<String> = vec!();
+    let mut flow_args: Vec<String> = vec![];
     if let Some(args) = matches.values_of("flow_args") {
         flow_args = args.map(|a| a.to_string()).collect();
     }
 
     SimpleLogger::init_prefix(matches.value_of("verbosity"), false);
 
-    debug!("'{}' version {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    debug!(
+        "'{}' version {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
     debug!("'flowclib' version {}", info::version());
 
     let cwd = env::current_dir().chain_err(|| "Could not get current working directory value")?;
-    let cwd_url =     Url::from_directory_path(cwd)
-        .map_err(|_|"Could not form a Url for the current working directory")?;
+    let cwd_url = Url::from_directory_path(cwd)
+        .map_err(|_| "Could not form a Url for the current working directory")?;
 
     let url = url_from_string(&cwd_url, matches.value_of("FLOW"))
         .chain_err(|| "Could not create a url for flow from the 'FLOW' command line parameter")?;
@@ -220,11 +249,13 @@ fn parse_args(matches: ArgMatches) -> Result<Options> {
         .chain_err(|| "Could not get or create the output directory")?;
 
     let lib_dirs = if matches.is_present("lib_dir") {
-        matches.values_of("lib_dir")
+        matches
+            .values_of("lib_dir")
             .chain_err(|| "Could not get the list of 'LIB_DIR' options specified")?
-            .map(|s| s.to_string()).collect()
+            .map(|s| s.to_string())
+            .collect()
     } else {
-        vec!()
+        vec![]
     };
 
     Ok(Options {
@@ -237,6 +268,6 @@ fn parse_args(matches: ArgMatches) -> Result<Options> {
         provided_implementations: matches.is_present("provided"),
         output_dir,
         stdin_file: matches.value_of("stdin").map(String::from),
-        lib_dirs
+        lib_dirs,
     })
 }
