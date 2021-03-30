274d68f54b61d7516de65e1761493e71017b23a2
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-03-30 16:37:36 +0200

    Rework provider crate to be Url based

--- provider/src/content/http_provider.rs
+++ provider/src/content/http_provider.rs
@@ -2,9 +2,8 @@ use curl::easy::{Easy2, Handler, WriteError};
 use log::debug;
 use url::Url;
 
-use crate::errors::*;
-
-use super::provider::Provider;
+use crate::Provider;
+use crate::Result;
 
 pub struct HttpProvider;
 
@@ -18,115 +17,133 @@ impl Handler for Collector {
 }
 
 impl Provider for HttpProvider {
-    fn resolve_url(&self, url_str: &str, default_filename: &str, extensions: &[&str]) -> Result<(String, Option<String>)> {
+    fn resolve_url(&self, url: &Url, default_filename: &str, extensions: &[&str]) -> Result<Url> {
         // Try the url directly first
-        if Self::resource_exists(url_str).is_ok() {
-            return Ok((url_str.into(), None));
+        if Self::resource_exists(url).is_ok() {
+            return Ok(url.clone());
         }
 
         // Try the url with possible extensions next
-        if let Ok(found) = Self::resource_by_extensions(url_str, extensions) {
-            return Ok((found, None));
+        if let Ok(found) = Self::resource_by_extensions(url, extensions) {
+            return Ok(found);
         }
 
-        // Attempting to find default file under this path
-        if let Ok(found) = Self::resource_by_extensions(&format!("{}/{}", url_str, default_filename), extensions) {
-            return Ok((found, None));
+        // Attempting to find default file under this path, with any of the valid extensions
+        let default_filename_url =
+            Url::parse(&format!("{}/{}", url.to_string(), default_filename))?;
+        if let Ok(found) = Self::resource_by_extensions(&default_filename_url, extensions) {
+            return Ok(found);
         }
 
-        // Attempt to find file with same name as final segment under that path
-        let url = Url::parse(url_str).map_err(|e| e.to_string())?;
+        // Attempt to find file with same name as final segment under that path, with any extension
         let mut segments = url.path_segments().ok_or("Could not get path segments")?;
-        let file_name = segments.next_back().ok_or("Could not get last path segment")?;
-        if let Ok(found) = Self::resource_by_extensions(&format!("{}/{}", url_str, file_name), extensions) {
-            return Ok((found, None));
+        let file_name = segments
+            .next_back()
+            .ok_or("Could not get last path segment")?;
+        let filename_url = Url::parse(&format!("{}/{}", url.to_string(), file_name))?;
+        if let Ok(found) = Self::resource_by_extensions(&filename_url, extensions) {
+            return Ok(found);
         }
 
-        bail!("Could not resolve the Url: '{}'", url_str)
+        bail!("Could not resolve the Url: '{}'", url)
     }
 
-    fn get_contents(&self, url_str: &str) -> Result<Vec<u8>> {
+    fn get_contents(&self, url: &Url) -> Result<Vec<u8>> {
         let mut easy = Easy2::new(Collector(Vec::new()));
-        easy.get(true).map_err(|_| "Could not perform GET action on content source")?;
-        easy.url(url_str).map_err(|_| "Could not set the url of the content source")?;
-        easy.perform().map_err(|_| "Could not perform the action")?;
-        match easy.response_code().map_err(|e| e.to_string())? {
+        easy.get(true)?;
+        easy.url(url.as_str())?;
+        easy.perform()?;
+        match easy.response_code()? {
             200..=299 => {
                 let contents = easy.get_ref();
                 Ok(contents.0.clone())
-            },
-            error_code => bail!("Response code: {} from '{}'", error_code, url_str)
+            }
+            error_code => bail!("Response code: {} from '{}'", error_code, url.as_str()),
         }
     }
 }
 
 impl HttpProvider {
-    fn resource_exists(url_str: &str) -> Result<()> {
-        debug!("Looking for resource '{}'", url_str);
+    fn resource_exists(url: &Url) -> Result<()> {
+        debug!("Looking for resource '{}'", url);
         let mut easy = Easy2::new(Collector(Vec::new()));
-        easy.nobody(true).map_err(|e| e.to_string())?;
+        easy.nobody(true)?;
 
-        easy.url(url_str).map_err(|e| e.to_string())?;
-        easy.perform().map_err(|e| e.to_string())?;
-        let response_code = easy.response_code().map_err(|e| e.to_string())?;
+        easy.url(url.as_str())?;
+        easy.perform()?;
 
         // Consider 301 - Permanently Moved as the resource NOT being at this Url
         // An option to consider is asking the request library to follow the redirect.
-        match response_code {
+        match easy.response_code()? {
             200..=299 => Ok(()),
-            error_code => bail!("Response code: {} from '{}'", error_code, url_str)
+            error_code => bail!("Response code: {} from '{}'", error_code, url.as_str()),
         }
     }
 
-    fn resource_by_extensions(resource: &str, extensions: &[&str]) -> Result<String> {
+    fn resource_by_extensions(resource: &Url, extensions: &[&str]) -> Result<Url> {
         // for that file path, try with all the allowed file extensions
         for extension in extensions {
-            let resource_with_extension = format!("{}.{}", resource, extension);
+            let resource_with_extension =
+                Url::parse(&format!("{}.{}", resource.as_str(), extension))?;
             if Self::resource_exists(&resource_with_extension).is_ok() {
                 return Ok(resource_with_extension);
             }
         }
 
-        bail!("No resources found at path '{}' with any of these extensions '{:?}'", resource, extensions)
+        bail!(
+            "No resources found at path '{}' with any of these extensions '{:?}'",
+            resource,
+            extensions
+        )
     }
 }
 
 #[cfg(test)]
 mod test {
+    use url::Url;
+
+    use crate::Provider;
+
     use super::HttpProvider;
-    use super::super::provider::Provider;
 
     #[test]
     fn resolve() {
         let provider = HttpProvider {};
-        let folder_url = "https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world/context.toml";
-        let full_url = provider.resolve_url(folder_url, "context", &[&"toml"])
-            .expect("Could not resolve url").0;
-        assert_eq!(folder_url, &full_url);
+        let folder_url = Url::parse("https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world/context.toml")
+            .expect("Could not form Url");
+        let full_url = provider
+            .resolve_url(&folder_url, "context", &[&"toml"])
+            .expect("Could not resolve url");
+        assert_eq!(folder_url.as_str(), full_url.as_str());
     }
 
     #[test]
     fn resolve_default() {
         let provider = HttpProvider {};
-        let folder_url = "https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world";
-        let full_url = provider.resolve_url(folder_url, "context", &[&"toml"])
-            .expect("Could not resolve url").0;
-        let mut expected = folder_url.to_string();
-        expected.push_str("/context.toml");
-        assert_eq!(full_url, expected);
+        let folder_url = Url::parse("https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world")
+            .expect("Could not form Url");
+        let expected_url = Url::parse("https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world/context.toml")
+            .expect("Could not form Url");
+        let full_url = provider
+            .resolve_url(&folder_url, "context", &[&"toml"])
+            .expect("Could not resolve url");
+        assert_eq!(full_url.as_str(), expected_url.as_str());
     }
 
     #[test]
     fn get_github_sample() {
         let provider: &dyn Provider = &HttpProvider;
-        let found = provider.get_contents("https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world/context.toml");
+        let url = Url::parse("https://raw.githubusercontent.com/andrewdavidmackenzie/flow/master/samples/hello-world/context.toml")
+            .expect("Could not form Url");
+        let found = provider.get_contents(&url);
         assert!(found.is_ok());
     }
 
     #[test]
     fn online_get_contents_file_not_found() {
         let provider: &dyn Provider = &HttpProvider;
-        let not_found = provider.get_contents("http://foo.com/no-such-file");
+        let url = Url::parse("http://foo.com/no-such-file").expect("Could not form Url");
+        let not_found = provider.get_contents(&url);
         assert!(not_found.is_err());
     }
 }
--- provider/src/content/mod.rs
+++ provider/src/content/mod.rs
@@ -1,7 +1,5 @@
 //! Content provider trait. It defines methods for for getting content of flows from files, http or library references.
-pub mod provider;
 /// The Content Provider for File contents
 pub mod file_provider;
 /// The Content Provider for Http contents
-mod http_provider;
-
+pub mod http_provider;
