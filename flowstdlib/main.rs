c247ac87920488e1bf3b1b8f0d8db6deb5450a36
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-04-18 15:30:40 +0200

    Add a shared lib_path.rs to check FLOW_LIB_PATH

--- flowstdlib/main.rs
+++ flowstdlib/main.rs
@@ -1,31 +1,13 @@
-use std::path::Path;
+use lib_path::check_flow_lib_path;
 
-use simpath::Simpath;
+mod lib_path;
 
 pub fn main() {
-    println!("'{}' version {} installed", env!("CARGO_CRATE_NAME"), env!("CARGO_PKG_VERSION"));
+    println!(
+        "'{}' version {}",
+        env!("CARGO_CRATE_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
     println!("For more details see: {}", env!("CARGO_PKG_HOMEPAGE"));
     check_flow_lib_path();
-}
-
-fn check_flow_lib_path() {
-    let parent = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap().display().to_string();
-    match std::env::var("FLOW_LIB_PATH") {
-        Err(_) => {
-            println!("'FLOW_LIB_PATH' is not set, so 'flowstdlib' will not be found by 'flowc' or 'flowr'.\n\
-             Set it to an appropriate value thus: export FLOW_LIB_PATH=\"{}\"", parent);
-        }
-        Ok(value) => {
-            let lib_path = Simpath::new_with_separator("FLOW_LIB_PATH", ',');
-            if !lib_path.contains(&parent) {
-                println!("'FLOW_LIB_PATH' is set to '{}'. But it does not contain the directory where 'flowstdlib' is\n\
-                            so 'flowstdlib' will not be found by 'flowc' or 'flowr'. \n\
-                            Add an entry for this directory thus: export FLOW_LIB_PATH=\"{},$FLOW_LIB_PATH\"",
-                         value, parent);
-            } else {
-                println!("'FLOW_LIB_PATH' is set to '{}' and correctly contains directory '{}'",
-                         value, parent);
-            }
-        }
-    }
 }
