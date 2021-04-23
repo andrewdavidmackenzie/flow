dca4b1580c750883ceeb49ced323d09f4b68e874
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-04-23 10:10:24 +0200

    Add tests to Route

--- flowstdlib/control/route/route.rs
+++ flowstdlib/control/route/route.rs
@@ -1,7 +1,7 @@
 use serde_json::Value;
 
 use flow_impl_derive::FlowImpl;
-use flowcore::{Implementation, RUN_AGAIN, RunAgain};
+use flowcore::{Implementation, RunAgain, RUN_AGAIN};
 
 #[derive(FlowImpl)]
 /// Route data to one or another based on a boolean control value.
@@ -21,5 +21,43 @@ impl Implementation for Route {
         }
 
         (Some(Value::Object(output_map)), RUN_AGAIN)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use flowcore::{Implementation, RUN_AGAIN};
+
+    #[test]
+    fn test_route_true() {
+        let router = &super::Route {} as &dyn Implementation;
+        let inputs = vec![json!(42), json!(true)];
+        let (output, run_again) = router.run(&inputs);
+        assert_eq!(run_again, RUN_AGAIN);
+
+        assert!(output.is_some());
+        let value = output.unwrap();
+        let map = value.as_object().unwrap();
+        assert_eq!(map.get("true").expect("No 'true' value in map"), &json!(42));
+        assert!(!map.contains_key("false"));
+    }
+
+    #[test]
+    fn test_route_false() {
+        let router = &super::Route {} as &dyn Implementation;
+        let inputs = vec![json!(42), json!(false)];
+        let (output, run_again) = router.run(&inputs);
+        assert_eq!(run_again, RUN_AGAIN);
+
+        assert!(output.is_some());
+        let value = output.unwrap();
+        let map = value.as_object().unwrap();
+        assert_eq!(
+            map.get("false").expect("No 'false' value in map"),
+            &json!(42)
+        );
+        assert!(!map.contains_key("true"));
     }
 }
