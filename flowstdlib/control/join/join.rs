31dec62a6bcc9f17a7d82bfbf8550bd0b54a8b0b
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-04-23 09:58:49 +0200

    Add tests to Join

--- flowstdlib/control/join/join.rs
+++ flowstdlib/control/join/join.rs
@@ -11,7 +11,24 @@ pub struct Join;
 impl Implementation for Join {
     fn run(&self, inputs: &[Value]) -> (Option<Value>, RunAgain) {
         let data = Some(inputs[0].clone());
-
+        // second input of 'control' is not used, it just "controls" the execution of this process
+        // via it's availability
         (data, RUN_AGAIN)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use flowcore::{Implementation, RUN_AGAIN};
+
+    #[test]
+    fn test_join() {
+        let joiner = &super::Join {} as &dyn Implementation;
+        let inputs = vec![json!(42), json!("OK")];
+        let (output, run_again) = joiner.run(&inputs);
+        assert_eq!(run_again, RUN_AGAIN);
+        assert_eq!(output.expect("No output value"), json!(42));
     }
 }
