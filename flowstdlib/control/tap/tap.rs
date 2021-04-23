ac05c525a3f46ac72729dba5c79ea1aff4fdfffd
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-04-23 10:33:37 +0200

    Add tests to Tap

--- flowstdlib/control/tap/tap.rs
+++ flowstdlib/control/tap/tap.rs
@@ -4,7 +4,7 @@ use flow_impl_derive::FlowImpl;
 use flowcore::{Implementation, RunAgain, RUN_AGAIN};
 
 #[derive(FlowImpl)]
-/// Control the flow of data (flow or disapear it) based on a boolean control value.
+/// Control the flow of data (flow or disappear it) based on a boolean control value.
 #[derive(Debug)]
 pub struct Tap;
 
@@ -18,5 +18,33 @@ impl Implementation for Tap {
         }
 
         (value, RUN_AGAIN)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use flowcore::{Implementation, RUN_AGAIN};
+
+    #[test]
+    fn test_tap_go() {
+        let tap = &super::Tap {} as &dyn Implementation;
+        let inputs = vec![json!("A"), json!(true)];
+        let (output, run_again) = tap.run(&inputs);
+        assert_eq!(run_again, RUN_AGAIN);
+
+        assert!(output.is_some());
+        let value = output.unwrap();
+        assert_eq!(value, json!("A"));
+    }
+
+    #[test]
+    fn test_tap_no_go() {
+        let tap = &super::Tap {} as &dyn Implementation;
+        let inputs = vec![json!("A"), json!(false)];
+        let (output, run_again) = tap.run(&inputs);
+        assert_eq!(run_again, RUN_AGAIN);
+        assert!(output.is_none());
     }
 }
