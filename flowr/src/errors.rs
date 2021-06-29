8b71c2a41bf2b47981879f1dabbf8b8c50c79d95
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-06-29 14:43:07 -0500

    Move error module into own file and allow missing docs on it only for flowc binary

---
+++ flowr/src/errors.rs
@@ -0,0 +1,16 @@
+#![allow(missing_docs)]
+
+pub use error_chain::bail;
+use error_chain::error_chain;
+
+error_chain! {
+    types {
+        Error, ErrorKind, ResultExt, Result;
+    }
+
+    foreign_links {
+        FlowCore(flowcore::errors::Error);
+        Runtime(flowrlib::errors::Error);
+        Io(std::io::Error);
+    }
+}
