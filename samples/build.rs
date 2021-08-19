fb193a03be793ce9fec55a44eca56df0a81450fc
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-08-19 10:11:44 +0200

    Remove SVG generation from samples as is now done by flowc when generating dot files.

--- samples/build.rs
+++ samples/build.rs
@@ -3,7 +3,6 @@ use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::{fs, io};
 
-use glob::{glob_with, MatchOptions};
 use simpath::{FileType, FoundType, Simpath};
 
 #[allow(clippy::collapsible_if)]
@@ -91,49 +90,6 @@ fn compile_sample(sample_dir: &Path, flowc: &Path) -> io::Result<()> {
             ))
         }
         _ => {}
-    }
-
-    generate_svgs(sample_dir)?;
-
-    Ok(())
-}
-
-/*
-   Generate SVG files from the .dot files created by flowc
-*/
-fn generate_svgs(sample_dir: &Path) -> io::Result<()> {
-    if let Ok(FoundType::File(dot)) = Simpath::new("PATH").find_type("dot", FileType::File) {
-        println!("Generating .dot.svg files from .dot files, using 'dot' command from $PATH");
-
-        let mut dot_command = Command::new(dot);
-        let options = MatchOptions {
-            case_sensitive: false,
-            ..Default::default()
-        };
-        let pattern = format!("{}/*.dot", sample_dir.to_str().unwrap());
-
-        for path in glob_with(&pattern, options).unwrap().flatten() {
-            let dot_child = dot_command
-                .args(vec!["-Tsvg", "-O", path.to_str().unwrap()])
-                .stdin(Stdio::inherit())
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .spawn()?;
-
-            let dot_output = dot_child.wait_with_output()?;
-            match dot_output.status.code() {
-                Some(0) => {}
-                Some(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "`dot` exited with non-zero status code",
-                    ))
-                }
-                _ => {}
-            }
-        }
-    } else {
-        println!("Could not find 'dot' command in $PATH so SVG generation skipped");
     }
 
     Ok(())
