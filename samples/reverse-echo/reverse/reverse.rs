bcd8454a50216f485e314e660e53b3c4891333fa
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-09-21 21:28:04 +0200

    Small refactor

--- samples/reverse-echo/reverse/reverse.rs
+++ samples/reverse-echo/reverse/reverse.rs
@@ -2,7 +2,6 @@ use flow_impl_derive::FlowImpl;
 use flowcore::Implementation;
 use serde_json::json;
 use serde_json::Value;
-use serde_json::Value::String as JsonString;
 
 #[derive(FlowImpl, Debug)]
 pub struct Reverser;
@@ -11,12 +10,14 @@ impl Implementation for Reverser {
     fn run(&self, inputs: &[Value]) -> (Option<Value>, bool) {
         let mut value = None;
 
-        let input = &inputs[0];
-        if let JsonString(ref s) = input {
-            value = Some(json!({
-                "reversed" : s.chars().rev().collect::<String>(),
-                "original": s
-            }));
+        if inputs.len() == 1 {
+            let input = &inputs[0];
+            if let Value::String(ref s) = input {
+                value = Some(json!({
+                    "reversed" : s.chars().rev().collect::<String>(),
+                    "original": s
+                }));
+            }
         }
 
         (value, true)
