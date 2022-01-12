ca1704b84bdc09da510b0b43a70a90504f0e19ce
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-01-12 18:05:01 +0100

    Fix mod.rs in generated manifest.rs

--- flowc/src/lib/compiler/rust_manifest.rs
+++ flowc/src/lib/compiler/rust_manifest.rs
@@ -49,7 +49,7 @@ pub fn write(lib_root: &Path, lib_manifest: &LibraryManifest, filename: &Path) -
     for module in modules {
         manifest_file.write_all(format!("\n/// functions from module '{}'", module).as_bytes())?;
         manifest_file.write_all(
-            format!("\n#[path=\"{}/{}/context\"]", lib_root.display(), module).as_bytes(),
+            format!("\n#[path=\"{}/{}/mod.rs\"]", lib_root.display(), module).as_bytes(),
         )?;
         manifest_file.write_all(format!("\npub mod {};\n", module).as_bytes())?;
     }
