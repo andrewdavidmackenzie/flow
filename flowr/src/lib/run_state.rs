4a4bea8fff10f2d915a0fb5b85010843853575ca
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-03-07 23:53:27 +0100

    Fix clippy warning

--- flowr/src/lib/run_state.rs
+++ flowr/src/lib/run_state.rs
@@ -1,8 +1,3 @@
-use std::collections::{HashMap, HashSet};
-use std::collections::VecDeque;
-use std::fmt;
-use std::time::Duration;
-
 use log::{debug, error, info, trace};
 use multimap::MultiMap;
 use serde_derive::{Deserialize, Serialize};
@@ -14,6 +9,10 @@ use flowcore::model::output_connection::OutputConnection;
 use flowcore::model::output_connection::Source::{Input, Output};
 use flowcore::model::runtime_function::RuntimeFunction;
 use flowcore::model::submission::Submission;
+use std::collections::{HashMap, HashSet};
+use std::collections::VecDeque;
+use std::fmt;
+use std::time::Duration;
 
 use crate::block::Block;
 #[cfg(feature = "debugger")]
@@ -1132,8 +1131,6 @@ impl fmt::Display for RunState {
 
 #[cfg(test)]
 mod test {
-    use std::sync::Arc;
-
     use serde_json::json;
     use serde_json::Value;
 
@@ -1143,6 +1140,7 @@ mod test {
     use flowcore::model::input::InputInitializer::Once;
     use flowcore::model::output_connection::{OutputConnection, Source};
     use flowcore::model::runtime_function::RuntimeFunction;
+    use std::sync::Arc;
 
     use crate::block::Block;
     use crate::debug_command::DebugCommand;
@@ -1302,14 +1300,11 @@ mod test {
     }
 
     #[cfg(feature = "debugger")]
-    fn dummy_debugger<'a>(server: &'a mut dyn DebugServer) -> Debugger<'a> {
+    fn dummy_debugger(server: &mut dyn DebugServer) -> Debugger {
         Debugger::new(server)
     }
 
     mod general_run_state_tests {
-        #[cfg(feature = "debugger")]
-        use std::collections::HashSet;
-
         #[cfg(feature = "debugger")]
         use multimap::MultiMap;
         #[cfg(any(feature = "debugger", feature = "metrics"))]
@@ -1317,6 +1312,8 @@ mod test {
 
         #[cfg(any(feature = "debugger", feature = "metrics"))]
         use flowcore::model::submission::Submission;
+        #[cfg(feature = "debugger")]
+                use std::collections::HashSet;
 
         #[cfg(any(feature = "debugger", feature = "metrics"))]
         use super::super::RunState;
