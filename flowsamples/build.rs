0d3b35331f54bcbc598433e41fc49db1202cd49d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-05-19 14:57:19 +0200

    Trigger sample rebuild on modifications to output dir (e.g. deletion of generated files etc) (#1261)
    Fixes #1194

--- flowsamples/build.rs
+++ flowsamples/build.rs
@@ -22,6 +22,7 @@ fn main() -> io::Result<()> {
                      sample_out_dir.display());
             fs::create_dir_all(sample_out_dir).expect("Could not create output dir");
             compile_sample(&e.path().to_string_lossy(), &sample_out_dir.to_string_lossy());
+            println!("cargo:rerun-if-changed={}", &sample_out_dir.to_string_lossy());
         }
     }
 
