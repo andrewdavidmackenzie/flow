adc7b01655778abdfd3a617564cf1ea35719dd41
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-08-05 09:34:04 +0200

    Increase coverage of ZIP library function (#1369)
    * Add a coverage target to Makefile
    * Reduce precision on test coverage
    * Delete coverage files before running tests with coverage
    * Add tests for zip()

--- flowstdlib/data/zip/zip.rs
+++ flowstdlib/data/zip/zip.rs
@@ -1,6 +1,7 @@
-use flowmacro::flow_function;
 use serde_json::json;
 use serde_json::Value;
+
+use flowmacro::flow_function;
 
 #[flow_function]
 fn _zip(inputs: &[Value]) -> Result<(Option<Value>, RunAgain)> {
@@ -9,4 +10,73 @@ fn _zip(inputs: &[Value]) -> Result<(Option<Value>, RunAgain)> {
     let tuples = left.iter().zip(right.iter());
     let tuples_vec: Vec<(&Value, &Value)> = tuples.collect();
     Ok((Some(json!(tuples_vec)), RUN_AGAIN))
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::{json, Value};
+
+    use super::_zip;
+
+    #[test]
+    fn zip_empty() {
+        let left = Value::Array(vec![]);
+        let right = Value::Array(vec![]);
+
+        let inputs = vec![left, right];
+
+        let (result, _) = _zip(&inputs).expect("_zip() failed");
+
+        let zipped_array = result.expect("Could not get the value from the output");
+
+        assert_eq!(zipped_array, Value::Array(vec!()));
+    }
+
+    #[test]
+    fn zip_happy() {
+        let left = json!(vec![1, 2]);
+        let right = json!(vec![3, 4]);
+
+        let inputs = vec![left, right];
+
+        let (result, _) = _zip(&inputs).expect("_zip() failed");
+
+        let zipped_array = result.expect("Could not get the value from the output");
+
+        assert_eq!(zipped_array, json!(vec![(1,3), (2,4)]));
+    }
+
+    #[test]
+    fn zip_invalid_left() {
+        let left = json!(1);
+        let right = json!(vec![3, 4]);
+
+        let inputs = vec![left, right];
+
+        assert!(_zip(&inputs).is_err());
+    }
+
+    #[test]
+    fn zip_invalid_right() {
+        let left = json!(vec![1, 2]);
+        let right = json!(3);
+
+        let inputs = vec![left, right];
+
+        assert!(_zip(&inputs).is_err());
+    }
+
+    #[test]
+    fn zip_unequal() {
+        let left = json!(vec![1, 2]);
+        let right = json!(vec![3]);
+
+        let inputs = vec![left, right];
+
+        let (result, _) = _zip(&inputs).expect("_zip() failed");
+
+        let zipped_array = result.expect("Could not get the value from the output");
+
+        assert_eq!(zipped_array, json!(vec![(1,3)]));
+    }
 }
