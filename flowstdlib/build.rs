4c01659ce833000e972e6fb0e3b2a356316da4c4
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-11-10 13:46:51 +0100

    Detect if a normal or packaging build

--- flowstdlib/build.rs
+++ flowstdlib/build.rs
@@ -5,13 +5,21 @@ use std::process::Command;
 
 // Build script to compile the flowstdlib library (compile WASM files and generate manifest) using flowc
 fn main() -> io::Result<()> {
-    let lib_root_dir = env!("CARGO_MANIFEST_DIR");
-    let workspace_root_dir = Path::new(lib_root_dir).parent().expect("Could not get parent directory");
-    let flowstdlib_out_dir = workspace_root_dir.join("target/flowstdlib");
-    let out_dir = flowstdlib_out_dir.to_str().expect("Could not convert to str");
+    let lib_root_dir_str = env!("CARGO_MANIFEST_DIR");
+    let lib_root_dir = Path::new(lib_root_dir_str);
+
+    let out_dir = if lib_root_dir.file_name().expect("Could not get file_name") == env!("CARGO_PKG_NAME") {
+        // local build from source folder - always compile to same folder "target/flowstdlib"
+        let workspace_root_dir = lib_root_dir.parent().expect("Could not get parent directory");
+        let dir = workspace_root_dir.join("target/flowstdlib");
+        dir.to_string_lossy().to_string()
+    } else {
+        // package build from a copy of source with version number in folder name
+        env::var("OUT_DIR").expect("Could not get OUT_DIR")
+    };
 
     // Tell Cargo that if any file changes it should rerun this build script
-    println!("cargo:rerun-if-changed={lib_root_dir}");
+    println!("cargo:rerun-if-changed={}", lib_root_dir_str);
 
     let mut command = Command::new("flowc");
     // flowc options:   -v info : to log output at INFO level,
@@ -25,10 +33,10 @@ fn main() -> io::Result<()> {
 
     // If the "wasm" feature is activated, then don't set "-n" and flowc will compile implementations to wasm.
     #[cfg(feature = "wasm")]
-    let command_args = vec!["-d", "-g", "-l", "-O", "-o", out_dir, lib_root_dir];
+    let command_args = vec!["-d", "-g", "-l", "-O", "-o", &out_dir, lib_root_dir_str];
     // If the "wasm" feature is NOT activated, then set "-n" (native only) flag so flowc will not compile to wasm
     #[cfg(not(feature = "wasm"))]
-    let command_args = vec!["-d", "-g", "-l", "-o", out_dir, "-n", lib_root_dir];
+    let command_args = vec!["-d", "-g", "-l", "-o", &out_dir, "-n", lib_root_dir_str];
 
     if !command
         .args(&command_args).status().expect("Could not get status").success() {
