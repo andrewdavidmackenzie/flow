b71ab2049f42d57e487455503b307d2d081ac983
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-12-19 16:39:59 +0100

    Comment formatting

--- flowstdlib/build.rs
+++ flowstdlib/build.rs
@@ -3,14 +3,17 @@ use std::io;
 use std::path::Path;
 use std::process::Command;
 
-// Build script to compile the flowstdlib library (compile WASM files and generate manifest) using flowc
+// Build script to compile the flowstdlib library (compile WASM files and generate manifest)
+// using flowc
 fn main() -> io::Result<()> {
     let lib_root_dir_str = env!("CARGO_MANIFEST_DIR");
     let lib_root_dir = Path::new(lib_root_dir_str);
 
-    let out_dir = if lib_root_dir.file_name().expect("Could not get file_name") == env!("CARGO_PKG_NAME") {
+    let out_dir = if lib_root_dir.file_name().expect("Could not get file_name")
+        == env!("CARGO_PKG_NAME") {
         // local build from source folder - always compile to same folder "target/flowstdlib"
-        let workspace_root_dir = lib_root_dir.parent().expect("Could not get parent directory");
+        let workspace_root_dir = lib_root_dir.parent()
+            .expect("Could not get parent directory");
         let dir = workspace_root_dir.join("target/flowstdlib");
         dir.to_string_lossy().to_string()
     } else {
@@ -31,15 +34,18 @@ fn main() -> io::Result<()> {
     //                  -n      : do not compile to WASM, only compile a native version of the lib
     //                  -l      : compile a flow library (not a flow) who's path is the last arg
 
-    // If the "wasm" feature is activated, then don't set "-n" and flowc will compile implementations to wasm.
+    // If the "wasm" feature is activated, then don't set "-n" and flowc will compile
+    // implementations to wasm.
     #[cfg(feature = "wasm")]
     let command_args = vec!["-d", "-g", "-l", "-O", "-o", &out_dir, lib_root_dir_str];
-    // If the "wasm" feature is NOT activated, then set "-n" (native only) flag so flowc will not compile to wasm
+    // If the "wasm" feature is NOT activated, then set "-n" (native only) flag so flowc will
+    // not compile to wasm
     #[cfg(not(feature = "wasm"))]
     let command_args = vec!["-d", "-g", "-l", "-o", &out_dir, "-n", lib_root_dir_str];
 
     if !command.args(&command_args).status()
-        .unwrap_or_else(|_| panic!("Could not get status of 'flowc {}'", command_args.join(" ")))
+        .unwrap_or_else(|_|
+            panic!("Could not get status of 'flowc {}'", command_args.join(" ")))
         .success() {
         std::process::exit(1);
     }
