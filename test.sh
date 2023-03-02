c10e05c9f1ddf76d44b1b037f7836253c1b642fd
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2023-03-02 10:08:39 +0100

    Test script renames log files

--- test.sh
+++ test.sh
@@ -3,13 +3,15 @@ flowc -C flowr/src/cli -c -d flowsamples/sequence-of-sequences
 while true;
 do
   # run the sample, sending number sequence from stdout to result.txt and logs to log.txt
-  flowr -v debug -t 4 -n flowsamples/sequence-of-sequences > result.txt 2> log.txt
+  flowr -v debug -t 2 -n flowsamples/sequence-of-sequences > result.txt 2> log.txt
   diff result.txt success.txt
   if [ $? -eq 1 ]
   then
-    echo "result.txt does not match success.txt, see log.txt for logs"
+    mv log.txt fail.txt
+    echo "result.txt does not match success.txt, see fail.txt for logs"
     exit 1
   else
-    echo "passed"
+    mv log.txt pass.txt
+    echo "passed, see log in pass.txt"
   fi
 done
