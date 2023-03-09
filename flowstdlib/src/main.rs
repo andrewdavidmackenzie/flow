bea380d4f1f3230108ee90987fd3b912c094b32f
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-03-09 22:18:37 +0100

    Correctly use search path for flowstdlib

--- flowstdlib/src/main.rs
+++ flowstdlib/src/main.rs
@@ -3,22 +3,22 @@
 
 use std::{env, io};
 
-use simpath::Simpath;
+use simpath::{FileType, FoundType, Simpath};
 
 /// Check the `FLOW_LIB_PATH` environment variable is set and that we can find the 'flowstdlib'
 /// lib directory using it.
 pub fn main() -> io::Result<()>{
     match env::var("FLOW_LIB_PATH") {
         Err(_) => {
-            println!("'FLOW_LIB_PATH' is not set. It must be set in order for the 'flowstdlib' directory to be found");
+            println!("'FLOW_LIB_PATH' is not set. It must be set in order for the 'flowstdlib' lib directory to be found");
         }
         Ok(value) => {
+                println!("'FLOW_LIB_PATH' is set to '{value}'");
             let lib_path = Simpath::new_with_separator("FLOW_LIB_PATH", ',');
-            if !lib_path.contains("flowstdlib") {
-                println!("'FLOW_LIB_PATH' is set to '{value}'. The 'flowstdlib' directory could not be found");
+            if let Ok(FoundType::Directory(found_at)) = lib_path.find_type("flowstdlib", FileType::Directory){
+                println!("The 'flowstdlib' lib directory was found at: '{}'", found_at.to_string_lossy());
             } else {
-                println!("'FLOW_LIB_PATH' is set to '{value}'. The 'flowstdlib' lib directory was found"
-                );
+                println!("The 'flowstdlib' lib directory could not be found");
             }
         }
     }
