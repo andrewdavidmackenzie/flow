ec517348c5fc33b514bf08cbb74cce12879a3bbd
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2023-03-17 22:50:44 +0000

    Deploying to gh-pages from @ andrewdavidmackenzie/flow@c438b5863bc90fda6f7ca34856d9fce4c26d39a8 🚀

--- code/doc/flowr/sidebar-items.js
+++ code/doc/flowr/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["client","client_and_server","client_only","get_bind_addresses","get_connect_addresses","get_flow_args","get_four_ports","get_matches","main","num_threads","parse_flow_url","run","server","server_only","setup_lib_search_path"],"mod":["cli","errors"]};
+window.SIDEBAR_ITEMS = {"fn":["client","client_and_server","client_only","get_bind_addresses","get_connect_addresses","get_flow_args","get_four_ports","get_lib_search_path","get_matches","main","num_threads","parse_flow_url","run","server","server_only"],"mod":["cli","errors"]};
