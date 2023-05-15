eeae22af62618aff6febec7eda5a5ff90b561400
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-05-15 17:49:53 +0200

    Optimize wasm size with wasm-strip (#1758)
    * wasm-strip to optimize .wasm file size
    
    * Refactor
    
    * Unrefactor

--- flowc/src/lib/compiler/compile_wasm.rs
+++ flowc/src/lib/compiler/compile_wasm.rs
@@ -93,9 +93,9 @@ pub fn compile_implementation(
    Try and run a command that may or may not be installed in the system thus:
    - create a temporary directory where the output file will be created
    - run the command: $command $wasm_path $args $temp_file
-   - return the path to the $temp_file
+   - copy the resulting $temp_file to the desired output path (possibly across file systems)
 */
-fn run_optional_command(wasm_path: &Path, command: &str, mut args: Vec<String>) -> Result<()> {
+fn run_optional_command(wasm_path: &Path, command: &str, args: Vec<&str>) -> Result<()> {
     if let Ok(FoundType::File(command_path)) =
         Simpath::new("PATH").find_type(command, FileType::File)
     {
@@ -109,13 +109,10 @@ fn run_optional_command(wasm_path: &Path, command: &str, mut args: Vec<String>) 
             .path()
             .join(wasm_path.file_name().ok_or("Could not get wasm filename")?);
         let mut command = Command::new(&command_path);
-        let mut command_args = vec![wasm_path.to_string_lossy().to_string()];
-        if !args.is_empty() {
-            command_args.append(&mut args);
-        }
-        command_args.append(&mut vec![temp_file_path.to_string_lossy().to_string()]);
+        command.arg(wasm_path);
+        command.args(&args);
+        command.arg(&temp_file_path);
         let child = command
-            .args(command_args)
             .stdin(Stdio::inherit())
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit());
@@ -133,30 +130,19 @@ fn run_optional_command(wasm_path: &Path, command: &str, mut args: Vec<String>) 
             )),
         }
 
-        // remove the temp dir
         fs::remove_dir_all(&tmp_dir)?;
     }
 
-    Ok(())
+    Ok(()) // No error if the command was not present
 }
 
 /*
-   Optimize a wasm file's size using equivalent of
-    wasm-gc $(file) -o $(file).gc && \
-    mv $(file).gc $(file) && \
-    wasm-snip $(file) -o $(file).snipped && \
-    mv $(file).snipped $(file) && \
-    wasm-gc $(file) -o $(file).gc && \
-    mv $(file).gc $(file) && \
-    wasm-opt $(file) -O4 --dce -o $(file).opt && \
-    mv $(file).opt $(file)
+   Optimize a wasm file's size using external tools that maybe installed on user's system
 */
 fn optimize_wasm_file_size(wasm_path: &Path) -> Result<()> {
-    run_optional_command(wasm_path, "wasm-snip", vec!["-o".into()])?;
-    run_optional_command(
-        wasm_path,
-        "wasm-opt",
-        vec!["-O4".into(), "--dce".into(), "-o".into()],
+    run_optional_command(wasm_path, "wasm-snip", vec!["-o"])?;
+    run_optional_command(wasm_path, "wasm-strip", vec!["-o"])?;
+    run_optional_command(wasm_path, "wasm-opt", vec!["-O4", "--dce", "-o"],
     )
 }
 
@@ -210,7 +196,7 @@ mod test {
 
     #[test]
     fn test_run_optional_non_existent() {
-        let _ = run_optional_command(Path::new("/tmp"), "foo", vec!["bar".into()]);
+        let _ = run_optional_command(Path::new("/tmp"), "foo", vec!["bar"]);
     }
 
     #[test]
@@ -230,7 +216,7 @@ mod test {
         let _ = run_optional_command(
             temp_file_path.as_path(),
             "cp",
-            vec!["--no-such-flag".into()],
+            vec!["--no-such-flag"],
         );
         assert!(temp_file_path.exists());
     }
