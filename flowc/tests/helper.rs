bb32af693ffcf37dd74a1241e6a5a41e3dc91fd6
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-05-18 17:36:55 +0200

    Nullify set_lib_search_path_to_project()

--- flowc/tests/helper.rs
+++ flowc/tests/helper.rs
@@ -6,16 +6,7 @@ use simpath::Simpath;
 use url::Url;
 
 pub fn set_lib_search_path_to_project() -> Simpath {
-    let mut lib_search_path = Simpath::new("lib_search_path");
-
-    // Add the parent directory of 'context' which is in flowr/src so it can be found
-    let root_str = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .expect("Could not get project root dir");
-    let runtime_parent = root_str.join("flowr/src");
-    lib_search_path.add_directory(runtime_parent.to_str().expect("Could not convert path to string"));
-
-    lib_search_path
+    Simpath::new("lib_search_path")
 }
 
 // Return a path to the context functions definitions provided by the CLI context of flowr
