928681490250b5b0adf001773145ef54664ed4f1
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-06-06 11:56:21 +0200

    Make script robust lack of binaries and certs installed

--- mac_test_runner.sh
+++ mac_test_runner.sh
@@ -1,8 +1,21 @@
-#!/usr/bin/env bash
-#code sign the test binary
-codesign -s self $1
+#!/bin/bash
 
-echo "MacOS test runner: Signed executable $1, now running it"
+# shellcheck disable=SC2046
+if [ $(which codesign) ]
+then
+  #echo "codesign detected"
+  if [ $(which security) ]
+  then
+    SELFCERT=$(security find-certificate -c "self" 2>&1 | grep "self")
+    if [ -n "$SELFCERT" ]
+    then
+      #echo "self certificate detected"
+      #code sign the test binary
+      codesign -s self "$1"
+      echo "MacOS test runner: Signed executable '$1'"
+    fi
+  fi
+fi
 
 #execute the test binary that was passed as an arg
 $1
