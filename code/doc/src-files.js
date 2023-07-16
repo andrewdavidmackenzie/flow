a5ac2aaad212d8035658736249dc5726ad66d82d
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2023-07-16 09:24:41 +0000

    Deploying to gh-pages from @ andrewdavidmackenzie/flow@881c685e9b3b4028fc4e5cc0a12efe51b6ad00db 🚀

--- code/doc/src-files.js
+++ code/doc/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "flowc":["",[],["errors.rs","flow_compile.rs","lib_build.rs","main.rs","source_arg.rs"]],\
 "flowcore":["",[["content",[],["file_provider.rs","http_provider.rs","mod.rs"]],["deserializers",[],["deserializer.rs","json_deserializer.rs","mod.rs","toml_deserializer.rs","yaml_deserializer.rs"]],["model",[],["connection.rs","datatype.rs","datatype_array_serde.rs","flow_definition.rs","flow_manifest.rs","function_definition.rs","input.rs","io.rs","lib_manifest.rs","metadata.rs","metrics.rs","mod.rs","name.rs","output_connection.rs","process.rs","process_reference.rs","route.rs","route_array_serde.rs","runtime_function.rs","submission.rs","validation.rs"]]],["errors.rs","lib.rs","meta_provider.rs","provider.rs","url_helper.rs"]],\
 "flowmacro":["",[],["lib.rs"]],\
@@ -10,4 +10,4 @@ var sourcesIndex = JSON.parse('{\
 "flowsamples":["",[],["main.rs"]],\
 "flowstdlib":["",[["control",[["compare_switch",[],["compare_switch.rs"]],["index",[],["index.rs"]],["join",[],["join.rs"]],["route",[],["route.rs"]],["select",[],["select.rs"]],["tap",[],["tap.rs"]]],["index_f.rs","mod.rs"]],["data",[["accumulate",[],["accumulate.rs"]],["append",[],["append.rs"]],["count",[],["count.rs"]],["duplicate",[],["duplicate.rs"]],["duplicate_rows",[],["duplicate_rows.rs"]],["enumerate",[],["enumerate.rs"]],["info",[],["info.rs"]],["multiply_row",[],["multiply_row.rs"]],["ordered_split",[],["ordered_split.rs"]],["remove",[],["remove.rs"]],["sort",[],["sort.rs"]],["split",[],["split.rs"]],["transpose",[],["transpose.rs"]],["zip",[],["zip.rs"]]],["mod.rs"]],["fmt",[["reverse",[],["reverse.rs"]],["to_json",[],["to_json.rs"]],["to_string",[],["to_string.rs"]]],["mod.rs"]],["math",[["add",[],["add.rs"]],["compare",[],["compare.rs"]],["divide",[],["divide.rs"]],["multiply",[],["multiply.rs"]],["range_split",[],["range_split.rs"]],["sqrt",[],["sqrt.rs"]],["subtract",[],["subtract.rs"]]],["mod.rs","range.rs","sequence.rs"]]],["errors.rs","lib.rs","manifest.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
