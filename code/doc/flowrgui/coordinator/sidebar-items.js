3a279d4ae7a33651c7519326112188e3d0bffc75
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2023-07-21 15:42:54 +0000

    Deploying to gh-pages from @ andrewdavidmackenzie/flow@1654aac114fbb677f3b2e7e6c2c184dee03d347d 🚀

--- code/doc/flowrgui/coordinator/sidebar-items.js
+++ code/doc/flowrgui/coordinator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CoordinatorState"],"fn":["coordinator","get_bind_addresses","get_connect_addresses","get_four_ports","start","subscribe"]};
+window.SIDEBAR_ITEMS = {"enum":["CoordinatorState"],"fn":["coordinator","get_bind_addresses","get_connect_addresses","get_four_ports","start_server","subscribe"]};
