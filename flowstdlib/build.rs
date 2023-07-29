aa1bd6bfc07500c344f3e6692838605a8e311aae
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-07-29 16:04:51 +0200

    Only rebuild when src changes.

--- flowstdlib/build.rs
+++ flowstdlib/build.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     let lib_root_dir_str = env!("CARGO_MANIFEST_DIR");
 
     // Tell Cargo that if any file changes it should rerun this build script
-    println!("cargo:rerun-if-changed={lib_root_dir_str}");
+    println!("cargo:rerun-if-changed={lib_root_dir_str}/src");
 
     let mut command = Command::new("flowc");
     // flowc options:   -v info : to log output at INFO level,
