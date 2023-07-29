3b2488cbbed65cbca375d330857259ab10144870
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-07-29 16:32:57 +0200

    Reduce rebuilds of flowstdlib

--- flowstdlib/build.rs
+++ flowstdlib/build.rs
@@ -5,11 +5,6 @@ use std::process::Command;
 // Build script to compile the flowstdlib library (compile WASM files and generate manifest)
 // using flowc
 fn main() -> io::Result<()> {
-    let lib_root_dir_str = env!("CARGO_MANIFEST_DIR");
-
-    // Tell Cargo that if any file changes it should rerun this build script
-    println!("cargo:rerun-if-changed={lib_root_dir_str}/src");
-
     let mut command = Command::new("flowc");
     // flowc options:   -v info : to log output at INFO level,
     //                  -n      : only build native implementations and not compile WASM files
@@ -20,11 +15,10 @@ fn main() -> io::Result<()> {
     //                  -n      : do not compile to WASM, only compile a native version of the lib
     //                  -l      : compile a flow library (not a flow) who's path is the last arg
 
-//    let home_dir = env::var("HOME").expect("Could not get $HOME");
-//    let out_dir = format!("{}/.flow/lib/flowstdlib", home_dir);
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:warning=flowstdlib built in '{out_dir}'");
-    let command_args = vec!["-d", "-g", "-l", "-O", "-o", &out_dir, lib_root_dir_str];
+    let home_dir = env::var("HOME").expect("Could not get $HOME");
+    let out_dir = format!("{}/.flow/lib/flowstdlib", home_dir);
+//    let out_dir = env::var("OUT_DIR").unwrap();
+    let command_args = vec!["-d", "-g", "-l", "-O", "-o", &out_dir, env!("CARGO_MANIFEST_DIR")];
 
     match command.args(&command_args).status() {
         Ok(stat) => {
