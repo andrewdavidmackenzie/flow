0311947ad35ace0b697a7c8608092b83b5190aba
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-07-30 15:30:09 +0200

    Add a test for the sequence flow

--- flowstdlib/src/lib.rs
+++ flowstdlib/src/lib.rs
@@ -112,4 +112,36 @@ to = \"stdout\"
         numbers.sort_unstable();
         assert_eq!(numbers, vec!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     }
+
+    #[test]
+    fn test_sequence_flow() {
+        let flow = "\
+flow = \"sequence_test\"
+
+[[process]]
+source = \"lib://flowstdlib/math/sequence\"
+input.start = { once = 1 }
+input.limit = { once = 10 }
+input.step = { once = 1 }
+
+[[process]]
+source = \"context://stdio/stdout\"
+
+[[connection]]
+from = \"sequence/sequence\"
+to = \"stdout\"
+";
+
+        let temp_dir = TempDir::new("flow").expect("Could not create TempDir").into_path();
+        let flow_filename = temp_dir.join("sequence_test.toml");
+        let mut flow_file =
+            File::create(&flow_filename).expect("Could not create lib manifest file");
+        flow_file.write_all(flow.as_bytes()).expect("Could not write data bytes to created flow file");
+
+        let stdout = execute_flow(flow_filename);
+
+        let numbers: Vec<i32> = stdout.lines().map(|l| l.parse::<i32>().expect("Not a number")).collect::<Vec<i32>>();
+        assert_eq!(numbers, vec!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
 }
