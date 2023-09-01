2884564ad2d978313693dd369e38b686e41cb78d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-09-01 15:24:35 +0200

    Change multiply_row to have multiple outputAdd row indexing to duplicate_rows and index combining in multiply_rows

--- flowstdlib/src/matrix/duplicate_rows/duplicate_rows.rs
+++ flowstdlib/src/matrix/duplicate_rows/duplicate_rows.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{json, Value};
 
 use flowcore::{RUN_AGAIN, RunAgain};
 use flowcore::errors::*;
@@ -7,95 +7,75 @@ use flowmacro::flow_function;
 #[flow_function]
 fn _duplicate_rows(inputs: &[Value]) -> Result<(Option<Value>, RunAgain)> {
     let mut output_matrix: Vec<Value> = vec![];
+    let mut row_indexes = vec![];
+    let mut output_map = serde_json::Map::new();
 
-    let factor = inputs[1].as_i64().ok_or("Could not get factor")?;
     let matrix = inputs[0].as_array().ok_or("Could not get matrix")?;
+    let factor = inputs[1].as_i64().ok_or("Could not get factor")?;
 
-    for row in matrix.iter() {
+    for (row_index, row) in matrix.iter().enumerate() {
         for _i in 0..factor {
             output_matrix.push(row.clone());
+            row_indexes.push(row_index)
         }
     }
 
-    Ok((Some(Value::Array(output_matrix)), RUN_AGAIN))
+    output_map.insert("matrix".into(), json!(output_matrix));
+    output_map.insert("row_indexes".into(), json!(row_indexes));
+
+    Ok((Some(Value::Object(output_map)), RUN_AGAIN))
 }
 
 #[cfg(test)]
 mod test {
-    use serde_json::{Number, Value};
     use serde_json::json;
 
     use super::_duplicate_rows;
 
     #[test]
     fn duplicate_2() {
-        let row0 = json!([1, 2]);
-        let row1 = json!([3, 4]);
-        let matrix = Value::Array(vec![row0, row1]);
+        let matrix = json!([[1, 2], [3, 4]]);
+        let duplication_factor = json!(2);
 
-        let inputs = vec![matrix, json!(2)];
+        let inputs = vec![matrix, duplication_factor];
 
         let (result, _) = _duplicate_rows(&inputs).expect("_duplicate_rows() failed");
 
-        let new_matrix = result.expect("Could not get the Value from the output");
-        let new_row0 = new_matrix[0].clone();
-        let new_row1 = new_matrix[1].clone();
-        let new_row2 = new_matrix[2].clone();
-        let new_row3 = new_matrix[3].clone();
+        let output = result.expect("Could not get the Value from the output");
 
-        assert_eq!(
-            new_row0,
-            Value::Array(vec!(
-                Value::Number(Number::from(1)),
-                Value::Number(Number::from(2))
-            ))
-        );
-        assert_eq!(
-            new_row1,
-            Value::Array(vec!(
-                Value::Number(Number::from(1)),
-                Value::Number(Number::from(2))
-            ))
-        );
-        assert_eq!(
-            new_row2,
-            Value::Array(vec!(
-                Value::Number(Number::from(3)),
-                Value::Number(Number::from(4))
-            ))
-        );
-        assert_eq!(
-            new_row3,
-            Value::Array(vec!(
-                Value::Number(Number::from(3)),
-                Value::Number(Number::from(4))
-            ))
-        );
+        let new_matrix = output.pointer("/matrix").expect("Could not get 'matrix' output");
+
+        assert_eq!(new_matrix[0], json!([1, 2]));
+        assert_eq!(new_matrix[1], json!([1, 2]));
+        assert_eq!(new_matrix[2], json!([3, 4]));
+        assert_eq!(new_matrix[3], json!([3, 4]));
+
+        let row_indexes = output.pointer("/row_indexes")
+            .expect("Could not get 'row_indexes' output");
+        assert_eq!(row_indexes[0], json!(0));
+        assert_eq!(row_indexes[1], json!(0));
+        assert_eq!(row_indexes[2], json!(1));
+        assert_eq!(row_indexes[3], json!(1));
     }
 
     #[test]
     fn duplicate_3() {
-        let row0 = json!([1, 2]);
-        let row1 = json!([3, 4]);
-        let matrix = Value::Array(vec![row0, row1]);
+        let matrix = json!([[1, 2], [3, 4]]);
+        let duplication_factor = json!(3);
 
-        let inputs = vec![matrix, json!(3)];
+        let inputs = vec![matrix, duplication_factor];
 
         let (result, _) = _duplicate_rows(&inputs).expect("_duplicate_rows() failed");
 
-        let new_matrix = result.expect("Could not get the Value from the output");
-        let new_row0 = new_matrix[0].clone();
-        let new_row1 = new_matrix[1].clone();
-        let new_row2 = new_matrix[2].clone();
-        let new_row3 = new_matrix[3].clone();
-        let new_row4 = new_matrix[4].clone();
-        let new_row5 = new_matrix[5].clone();
+        let output = result.expect("Could not get the Value from the output");
 
-        assert_eq!(new_row0, json!([1, 2]));
-        assert_eq!(new_row1, json!([1, 2]));
-        assert_eq!(new_row2, json!([1, 2]));
-        assert_eq!(new_row3, json!([3, 4]));
-        assert_eq!(new_row4, json!([3, 4]));
-        assert_eq!(new_row5, json!([3, 4]));
+        let new_matrix = output.pointer("/matrix").expect("Could not get 'matrix' output");
+
+        assert_eq!(new_matrix[0], json!([1, 2]));
+        assert_eq!(new_matrix[1], json!([1, 2]));
+        assert_eq!(new_matrix[2], json!([1, 2]));
+        assert_eq!(new_matrix[3], json!([3, 4]));
+        assert_eq!(new_matrix[4], json!([3, 4]));
+        assert_eq!(new_matrix[5], json!([3, 4]));
     }
 }
--- flowstdlib/src/matrix/multiply_row/multiply_row.rs
+++ flowstdlib/src/matrix/multiply_row/multiply_row.rs
@@ -9,18 +9,21 @@ fn _multiply_row(inputs: &[Value]) -> Result<(Option<Value>, RunAgain)> {
     let mut product = 0;
     let mut output_map = serde_json::Map::new();
 
-    let row0 = inputs[0].as_array().ok_or("Could not get row0")?;
-    let row1 = inputs[1].as_array().ok_or("Could not get row1")?;
+    let a = inputs[0].as_array().ok_or("Could not get a")?;
+    let a_index = inputs[1].as_u64();
+    let b = inputs[2].as_array().ok_or("Could not get b")?;
+    let b_index = inputs[3].as_u64();
 
-    for index in 0..row0.len() {
-        if let Some(row0_entry) = row0[index].as_i64() {
-            if let Some(row1_entry) = row1[index].as_i64() {
+    for index in 0..a.len() {
+        if let Some(row0_entry) = a[index].as_i64() {
+            if let Some(row1_entry) = b[index].as_i64() {
                 product += row0_entry * row1_entry;
             }
         }
     }
 
     output_map.insert("product".into(), json!(product));
+    output_map.insert("a_b_index".into(), json!([a_index, b_index]));
 
     Ok((Some(Value::Object(output_map)), RUN_AGAIN))
 }
@@ -28,21 +31,25 @@ fn _multiply_row(inputs: &[Value]) -> Result<(Option<Value>, RunAgain)> {
 #[cfg(test)]
 mod test {
     use serde_json::json;
-    use serde_json::Value;
 
     use super::_multiply_row;
 
     #[test]
     fn multiply_row() {
-        let row0 = Value::Array(vec![json!(1), json!(2)]);
-        let row1 = Value::Array(vec![json!(3), json!(4)]);
+        let a = json!([1,2]);
+        let a_index = json!(0);
+        let b = json!([3,4]);
+        let b_index = json!(1);
 
-        let inputs = vec![row0, row1];
+        let inputs = vec![a, a_index, b, b_index];
 
         let (result, _) = _multiply_row(&inputs).expect("_multiply_row() failed");
 
         let output = result.expect("Could not get the Value from the output");
 
-        assert_eq!(output.pointer("/product").expect("Could not get 'product' output"), &json!(11));
+        assert_eq!(output.pointer("/product").expect("Could not get 'product' output"),
+                   &json!(11));
+        assert_eq!(output.pointer("/a_b_index").expect("Could not get 'product' output"),
+                   &json!([0,1]));
     }
 }
