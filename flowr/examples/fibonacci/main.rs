c65001bf2460f0cf01f271df3e7e96d52d441ea8
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-09-19 00:04:43 +0200

    Run main() to exercise examples

--- flowr/examples/fibonacci/main.rs
+++ flowr/examples/fibonacci/main.rs
@@ -7,12 +7,17 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    use std::env;
+    use std::path::PathBuf;
     use serial_test::serial;
 
     #[test]
     #[serial]
     fn test_fibonacci_example() {
-        utilities::test_example(file!(), "flowrcli",
-                                false, true);
+        let _ = env::set_current_dir(PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .parent().expect("Could not cd into flow directory"));
+
+        super::main();
+        utilities::check_test_output(file!());
     }
 }
