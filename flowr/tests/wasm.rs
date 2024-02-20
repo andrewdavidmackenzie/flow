9f4cd80aef9785c5182f2d08a4a1864657120a27
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-02-20 10:37:35 +0100

    Wrap line

--- flowr/tests/wasm.rs
+++ flowr/tests/wasm.rs
@@ -1,4 +1,5 @@
 #[test]
 fn test_fibonacci_wasm_example() {
-    utilities::test_example("flowr/examples/fibonacci/main.rs", "flowrcli", false, false);
+    utilities::test_example("flowr/examples/fibonacci/main.rs",
+                            "flowrcli", false, false);
 }
