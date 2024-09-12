c23098c7e81cf944f6a8df57a983eecaf27f965d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-09-12 21:51:38 +0200

    Disable wasm tests

--- flowr/examples/reverse-echo/main.rs
+++ flowr/examples/reverse-echo/main.rs
@@ -9,6 +9,7 @@ mod test {
     use std::path::PathBuf;
 
     #[test]
+    #[ignore]
     fn test_reverse_echo_example() {
         let _ = env::set_current_dir(PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .parent().expect("Could not cd into flow directory"));
--- flowr/tests/wasm.rs
+++ flowr/tests/wasm.rs
@@ -1,6 +1,7 @@
 #![allow(missing_docs)]
 
 #[test]
+#[ignore]
 fn test_fibonacci_wasm_example() {
     utilities::test_example("flowr/examples/fibonacci/main.rs",
                             "flowrcli", false, false);
